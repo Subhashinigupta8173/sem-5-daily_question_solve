30b467c43e752c4b6397f4e5e74e4126e975ff1b
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2025-08-05 22:25:38 +0530

    Create 40-CombinationSumIi

---
+++ 40-CombinationSumIi/40-CombinationSumIi.java
@@ -0,0 +1,48 @@
+// Last updated: 5/8/2025, 10:25:38 pm
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(candidates); 
+        backtrack(result, new ArrayList<>(), candidates, target, 0);
+        return result;
+    }
+
+    private void backtrack(List<List<Integer>> result, List<Integer> tempList, int[] candidates, int remain, int start) {
+        if (remain < 0) return; 
+        else if (remain == 0) result.add(new ArrayList<>(tempList)); 
+        else {
+            for (int i = start; i < candidates.length; i++) {
+                if (i > start && candidates[i] == candidates[i - 1]) continue; 
+                tempList.add(candidates[i]); 
+                backtrack(result, tempList, candidates, remain - candidates[i], i + 1); 
+                tempList.remove(tempList.size() - 1); 
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println();
+
+       
+        int n = scanner.nextInt();
+
+        if (n <= 0) {
+            System.out.println();
+            return;
+        }
+
+        int[] candidates = new int[n];
+
+        System.out.println();
+        for (int i = 0; i < n; i++) {
+            candidates[i] = scanner.nextInt();
+        }
+        int target = scanner.nextInt();
+
+        Solution solution = new Solution();
+        List<List<Integer>> result = solution.combinationSum2(candidates, target);
+
+        System.out.println( result);
+    }
+}
