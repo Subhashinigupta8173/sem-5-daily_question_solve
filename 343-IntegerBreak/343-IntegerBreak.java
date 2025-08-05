580bdc34b807179adcfd2b4d9f74f265f9dded95
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2025-08-05 22:24:43 +0530

    Create 343-IntegerBreak

---
+++ 343-IntegerBreak/343-IntegerBreak.java
@@ -0,0 +1,15 @@
+// Last updated: 5/8/2025, 10:24:43 pm
+class Solution {
+    public int integerBreak(int n) {
+        if (n == 2) return 1;
+        if (n == 3) return 2;
+
+        int product = 1;
+        while (n > 4) {
+            product *= 3;
+            n -= 3;
+        }
+        product *= n;
+        return product;
+    }
+}
