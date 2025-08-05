8bd00fb3a6073160b37202de5467531688601235
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2025-08-05 22:24:46 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.cpp
@@ -0,0 +1,13 @@
+// Last updated: 5/8/2025, 10:24:46 pm
+class Solution {
+public:
+    bool canWinNim(int n) {
+        if(n%4==0)
+        {
+            return false;
+
+        }
+        return true;
+        
+    }
+};
