129368eab211450bef04283cdd28ead69abd7d06
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2025-08-05 22:24:50 +0530

    Create 275-HIndexIi

---
+++ 275-HIndexIi/275-HIndexIi.java
@@ -0,0 +1,18 @@
+// Last updated: 5/8/2025, 10:24:51 pm
+public class Solution {
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        int left = 0, right = n - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (citations[mid] >= n - mid) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        return n - left;
+    }
+}
