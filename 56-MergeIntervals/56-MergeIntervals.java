bc5fb073a8d19ff72c4f1883368a84bb8e8e5489
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2025-08-05 23:44:33 +0530

    File updated at 5/8/2025, 11:44:34 pm

--- 56-MergeIntervals/56-MergeIntervals.java
+++ 56-MergeIntervals/56-MergeIntervals.java
@@ -1,26 +1,8 @@
-// Last updated: 5/8/2025, 11:33:51 pm
-import java.util.*;
-
+// Last updated: 5/8/2025, 11:44:34 pm
 class Solution {
-    public int[][] merge(int[][] intervals) {
+    public void sortColors(int[] nums) {
+        Arrays.sort(nums);
         
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-
-        List<int[]> merged = new ArrayList<>();
-
-        
-        for (int i = 0; i < intervals.length; i++) {
-            int start = intervals[i][0];
-            int end = intervals[i][1];
-
-            if (merged.isEmpty() || merged.get(merged.size() - 1)[1] < start) {
-                merged.add(new int[]{start, end});
-            } else {
-                merged.get(merged.size() - 1)[1] = Math.max(merged.get(merged.size() - 1)[1], end);
-            }
-        }
-
-     
-        return merged.toArray(new int[merged.size()][]);
     }
+    
 }
