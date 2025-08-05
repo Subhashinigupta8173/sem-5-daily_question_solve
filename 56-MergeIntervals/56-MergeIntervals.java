35e7dccabc3a110e068ccf1afec63df3ea38863d
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2025-08-06 00:07:47 +0530

    File updated at 6/8/2025, 12:07:48 am

--- 56-MergeIntervals/56-MergeIntervals.java
+++ 56-MergeIntervals/56-MergeIntervals.java
@@ -1,8 +1,21 @@
-// Last updated: 5/8/2025, 11:44:34 pm
+// Last updated: 6/8/2025, 12:07:48 am
 class Solution {
-    public void sortColors(int[] nums) {
-        Arrays.sort(nums);
-        
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>(); 
+        List<Integer> current = new ArrayList<>();      
+
+        generateSubsets(0, nums, current, result);
+
+        return result;
     }
-    
+
+   
+    private void generateSubsets(int index, int[] nums, List<Integer> current, List<List<Integer>> result) {
+        result.add(new ArrayList<>(current)); 
+        for (int i = index; i < nums.length; i++) {
+            current.add(nums[i]); 
+            generateSubsets(i + 1, nums, current, result); 
+            current.remove(current.size() - 1); 
+        }
+    }
 }
