726846cb61b6cb52310791b8da6ff27a21d75736
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2025-09-03 08:07:40 +0530

    File updated at 3/9/2025, 8:07:40 am

--- 70-ClimbingStairs/70-ClimbingStairs.java
+++ 70-ClimbingStairs/70-ClimbingStairs.java
@@ -1,23 +1,6 @@
-// Last updated: 3/9/2025, 8:06:52 am
-import java.util.HashSet;
-
+// Last updated: 3/9/2025, 8:07:40 am
 class Solution {
-    public boolean isHappy(int n) {
-        HashSet<Integer> seen = new HashSet<>();
-        while (n != 1 && !seen.contains(n)) {
-            seen.add(n);
-            n = getNext(n);
-        }
-        return n == 1;
-    }
-    
-    private int getNext(int n) {
-        int sum = 0;
-        while (n > 0) {
-            int digit = n % 10;
-            sum += digit * digit;
-            n /= 10;
-        }
-        return sum;
+    public boolean isPowerOfTwo(int n) {
+        return n > 0 && (n & (n - 1)) == 0;
     }
 }
