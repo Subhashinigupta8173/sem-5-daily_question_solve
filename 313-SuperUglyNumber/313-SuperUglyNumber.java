97d7cff2ff9cecdb7e1e7fc681bab36fe84aed24
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2025-09-03 08:27:39 +0530

    File updated at 3/9/2025, 8:27:39 am

--- 313-SuperUglyNumber/313-SuperUglyNumber.java
+++ 313-SuperUglyNumber/313-SuperUglyNumber.java
@@ -1,34 +1,20 @@
-// Last updated: 3/9/2025, 8:24:10 am
+// Last updated: 3/9/2025, 8:27:39 am
 class Solution {
-    public int nthSuperUglyNumber(int n, int[] primes) {
-        long[] ugly = new long[n];
-        ugly[0] = 1; 
-        
-        int k = primes.length;
-        int[] idx = new int[k]; 
-        long[] nextMultiple = new long[k];
-        
-        for (int i = 0; i < k; i++) {
-            nextMultiple[i] = primes[i];
+    public String toHex(int num) {
+        if (num == 0) return "0"; 
+
+        char[] hexChars = "0123456789abcdef".toCharArray();
+        StringBuilder sb = new StringBuilder();
+
+       
+        long n = num & 0xFFFFFFFFL;
+
+        while (n != 0) {
+            int digit = (int)(n & 0xF); 
+            sb.append(hexChars[digit]);
+            n >>= 4;
         }
-        
-        for (int i = 1; i < n; i++) {
-            long nextUgly = Long.MAX_VALUE;
-            
-            for (int j = 0; j < k; j++) {
-                nextUgly = Math.min(nextUgly, nextMultiple[j]);
-            }
-            
-            ugly[i] = nextUgly;
-            
-            for (int j = 0; j < k; j++) {
-                if (nextUgly == nextMultiple[j]) {
-                    idx[j]++;
-                    nextMultiple[j] = ugly[idx[j]] * primes[j];
-                }
-            }
-        }
-        
-        return (int) ugly[n - 1]; 
+
+        return sb.reverse().toString(); 
     }
 }
