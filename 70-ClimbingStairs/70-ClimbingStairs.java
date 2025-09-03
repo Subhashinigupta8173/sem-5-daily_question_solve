5832e44e7c82bc6f9c6a7ac130334f2bcd97f02e
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2025-09-03 08:11:34 +0530

    File updated at 3/9/2025, 8:11:34 am

--- 70-ClimbingStairs/70-ClimbingStairs.java
+++ 70-ClimbingStairs/70-ClimbingStairs.java
@@ -1,15 +1,7 @@
-// Last updated: 3/9/2025, 8:10:43 am
+// Last updated: 3/9/2025, 8:11:34 am
 class Solution {
-    public int numSquares(int n) {
-        int[] dp = new int[n + 1];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[0] = 0;
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j * j <= i; j++) {
-                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
-            }
-        }
-        return dp[n];
+    public boolean isPowerOfThree(int n) {
+        
+        return n > 0 && 1162261467 % n == 0;
     }
 }
