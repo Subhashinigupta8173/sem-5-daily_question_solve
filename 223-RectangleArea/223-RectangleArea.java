b750461a31379b56479ac72439bc4fa2309d06d2
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2025-09-03 08:19:41 +0530

    File updated at 3/9/2025, 8:19:41 am

--- 223-RectangleArea/223-RectangleArea.java
+++ 223-RectangleArea/223-RectangleArea.java
@@ -1,28 +1,19 @@
-// Last updated: 3/9/2025, 8:17:56 am
+// Last updated: 3/9/2025, 8:19:41 am
 class Solution {
-    private static final int MOD = 1337;
-
-    public int superPow(int a, int[] b) {
-        return helper(a, b, b.length);
-    }
-
-    private int helper(int a, int[] b, int k) {
-        if (k == 0) return 1; 
-
-        int lastDigit = b[k - 1];
-        int part1 = powMod(a, lastDigit);         
-        int part2 = powMod(helper(a, b, k - 1), 10);
-
-        return (part1 * part2) % MOD;
-    }
-
- 
-    private int powMod(int a, int k) {
-        a %= MOD;
-        int result = 1;
-        for (int i = 0; i < k; i++) {
-            result = (result * a) % MOD;
+    public List<String> fizzBuzz(int n) {
+        List<String> result = new ArrayList<>();
+        
+        for (int i = 1; i <= n; i++) {
+            StringBuilder sb = new StringBuilder();
+            
+            if (i % 3 == 0) sb.append("Fizz");
+            if (i % 5 == 0) sb.append("Buzz");
+            
+            if (sb.length() == 0) sb.append(i);
+            
+            result.add(sb.toString());
         }
+        
         return result;
     }
 }
