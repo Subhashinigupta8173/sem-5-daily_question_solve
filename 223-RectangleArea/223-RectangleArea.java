289ddc483dc3206aa69ccd4ba122d7fa3b153a0b
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2025-09-03 08:17:42 +0530

    File updated at 3/9/2025, 8:17:42 am

--- 223-RectangleArea/223-RectangleArea.java
+++ 223-RectangleArea/223-RectangleArea.java
@@ -1,11 +1,28 @@
-// Last updated: 3/9/2025, 8:15:41 am
+// Last updated: 3/9/2025, 8:17:42 am
 class Solution {
-    public int getSum(int a, int b) {
-        while (b != 0) {
-            int carry = (a & b) << 1; 
-            a = a ^ b;               
-            b = carry;               
+    private static final int MOD = 1337;
+
+    public int superPow(int a, int[] b) {
+        return helper(a, b, b.length);
+    }
+
+    private int helper(int a, int[] b, int k) {
+        if (k == 0) return 1; 
+
+        int lastDigit = b[k - 1];
+        int part1 = powMod(a, lastDigit);         
+        int part2 = powMod(helper(a, b, k - 1), 10);
+
+        return (part1 * part2) % MOD;
+    }
+
+ 
+    private int powMod(int a, int k) {
+        a %= MOD;
+        int result = 1;
+        for (int i = 0; i < k; i++) {
+            result = (result * a) % MOD;
         }
-        return a;
+        return result;
     }
 }
