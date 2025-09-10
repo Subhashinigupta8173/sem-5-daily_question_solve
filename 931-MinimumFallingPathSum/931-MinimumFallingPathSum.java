eae26eeaedac564421f554294691c847262e935f
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2025-09-10 12:37:41 +0530

    File updated at 10/9/2025, 12:37:41 pm

--- 931-MinimumFallingPathSum/931-MinimumFallingPathSum.java
+++ 931-MinimumFallingPathSum/931-MinimumFallingPathSum.java
@@ -1,33 +1,38 @@
-// Last updated: 10/9/2025, 12:24:28 pm
+// Last updated: 10/9/2025, 12:37:41 pm
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
-        // int[][] matrix = {{2, 1, 3}, {6, 5, 4}, {7, 8, 9}};
-        int ans = Integer.MAX_VALUE;
-        int[][] dp=new int[matrix.length][matrix[0].length];
-        for(int[] a:dp) {
-        	Arrays.fill(a,-66666);
-        }
-        for (int col = 0; col < matrix[0].length; col++) {
-            ans = Math.min(ans, Minimum_Falling_Path(matrix, 0, col,dp));
-        }
+        // int [][] matrix= {{2,1,3},{6,5,4},{7,8,9}};
+		int ans=Integer.MAX_VALUE;
+		int [][] dp=new int[matrix.length][matrix[0].length];
+		for(int[] a:dp) {
+			Arrays.fill(a, -66666);
+		}
+		for(int col=0; col<matrix[0].length; col++) {
+			ans=Math.min(ans, Minimum_falling_path(matrix,0,col,dp));
+		}
         return ans;
-    }
 
-    public static int Minimum_Falling_Path(int[][] matrix, int cr, int cc,int[][] dp) {
-        if (cc < 0 || cc >= matrix[0].length) {
-            return Integer.MAX_VALUE;
-        }
-        if (cr == matrix.length - 1) {
-            return matrix[cr][cc];
-        }
-        if(dp[cr][cc]!=-66666) {
-        	return dp[cr][cc];
-        }
+	}
+	public static int Minimum_falling_path(int[][] matrix,int cr,int cc,int [] [] dp ) {
+		if(cc<0 | cc>=matrix[0].length) {
+			return Integer.MAX_VALUE;
+		}
+		if(cr==matrix.length-1) {
+			return matrix[cr][cc];
+		}
+		if(dp[cr][cc]!=-66666) {
+			return dp[cr][cc];
+		}
+		int ans=Integer.MAX_VALUE;
+		for(int i=0; i<matrix[0].length; i++) {
+			if(i==cc) {
+				continue;
+			}
+			ans=Math.min(ans,Minimum_falling_path(matrix,cr+1,i,dp));
+		}
+	   return dp[cr][cc]=ans+matrix[cr][cc];
+		
+	}
+        
 
-        int ld = Minimum_Falling_Path(matrix, cr + 1, cc - 1,dp);
-        int rd = Minimum_Falling_Path(matrix, cr + 1, cc + 1,dp);
-        int d = Minimum_Falling_Path(matrix, cr + 1, cc,dp);
-
-        return dp[cr][cc]=Math.min(d, Math.min(ld, rd)) + matrix[cr][cc];
-    }
 }
