7a92daf22ce97d3bbddcaaf6aae14f7b1d33711d
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2025-09-22 21:17:29 +0530

    File updated at 22/9/2025, 9:17:29 pm

--- 137-SingleNumberII/137-SingleNumberII.java
+++ 137-SingleNumberII/137-SingleNumberII.java
@@ -1,28 +1,25 @@
-// Last updated: 22/9/2025, 9:15:38 pm
+// Last updated: 22/9/2025, 9:17:29 pm
+import java.util.*;
+
 class Solution {
-    public int maxProfit(int k, int[] prices) {
-        int n = prices.length;
-        if (n == 0) return 0;
-        
-        // If k is large, just sum all positive differences
-        if (k >= n / 2) {
-            int profit = 0;
-            for (int i = 1; i < n; i++) {
-                if (prices[i] > prices[i-1]) profit += prices[i] - prices[i-1];
-            }
-            return profit;
-        }
-        
-        int[][] dp = new int[k+1][n];
-        
-        for (int i = 1; i <= k; i++) {
-            int maxDiff = -prices[0];
-            for (int j = 1; j < n; j++) {
-                dp[i][j] = Math.max(dp[i][j-1], prices[j] + maxDiff);
-                maxDiff = Math.max(maxDiff, dp[i-1][j] - prices[j]);
+    public List<String> summaryRanges(int[] nums) {
+        List<String> res = new ArrayList<>();
+        if (nums == null || nums.length == 0) return res;
+
+        int start = nums[0];
+
+        for (int i = 1; i <= nums.length; i++) {
+            // check if end of range
+            if (i == nums.length || nums[i] != nums[i-1] + 1) {
+                if (start == nums[i-1]) {
+                    res.add(String.valueOf(start)); // single number
+                } else {
+                    res.add(start + "->" + nums[i-1]); // range
+                }
+                if (i < nums.length) start = nums[i]; // start new range
             }
         }
-        
-        return dp[k][n-1];
+
+        return res;
     }
 }
