dbfc0b9029d33cf301828095b8a81aeff2ad5a12
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2025-09-22 19:11:47 +0530

    File updated at 22/9/2025, 7:11:47 pm

--- 36-ValidSudoku/36-ValidSudoku.java
+++ 36-ValidSudoku/36-ValidSudoku.java
@@ -1,4 +1,4 @@
-// Last updated: 22/9/2025, 7:11:33 pm
+// Last updated: 22/9/2025, 7:11:47 pm
 class Solution {
     public boolean isValidSudoku(char[][] board) {
         HashSet<Character>[] rows = new HashSet[9];
