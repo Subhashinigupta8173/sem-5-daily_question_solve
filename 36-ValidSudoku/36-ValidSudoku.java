45be076be41071cf1e860a43d68d1fe150c57374
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2025-09-22 19:09:16 +0530

    Create 36-ValidSudoku

---
+++ 36-ValidSudoku/36-ValidSudoku.java
@@ -0,0 +1,39 @@
+// Last updated: 22/9/2025, 7:09:15 pm
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<Character>[] rows = new HashSet[9];
+        HashSet<Character>[] cols = new HashSet[9];
+        HashSet<Character>[] boxes = new HashSet[9];
+
+        for (int i = 0; i < 9; i++) {
+            rows[i] = new HashSet<>();
+            cols[i] = new HashSet<>();
+            boxes[i] = new HashSet<>();
+        }
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                char val = board[r][c];
+                
+                if (val == '.') continue; // skip empty
+                
+                // Row check
+                if (rows[r].contains(val)) return false;
+                rows[r].add(val);
+                
+                // Column check
+                if (cols[c].contains(val)) return false;
+                cols[c].add(val);
+                
+                // Box check
+                int boxIndex = (r / 3) * 3 + (c / 3);
+                if (boxes[boxIndex].contains(val)) return false;
+                boxes[boxIndex].add(val);
+            }
+        }
+        return true;
+
+        
+
+        
+    }
+}
