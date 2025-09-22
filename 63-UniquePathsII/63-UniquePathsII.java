8625d8c53e92eda5872f0fdf925166478e9d3953
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2025-09-22 20:58:20 +0530

    File updated at 22/9/2025, 8:58:19 pm

--- 63-UniquePathsII/63-UniquePathsII.java
+++ 63-UniquePathsII/63-UniquePathsII.java
@@ -1,47 +1,25 @@
-// Last updated: 22/9/2025, 8:57:07 pm
+// Last updated: 22/9/2025, 8:58:19 pm
 class Solution {
-    public void setZeroes(int[][] matrix) {
-        int m = matrix.length, n = matrix[0].length;
-        boolean firstRowHasZero = false;
-        boolean firstColHasZero = false;
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
 
-        // Check if first row has zero
-        for (int j = 0; j < n; j++) {
-            if (matrix[0][j] == 0) firstRowHasZero = true;
-        }
+        int left = 0, right = m * n - 1;
 
-        // Check if first column has zero
-        for (int i = 0; i < m; i++) {
-            if (matrix[i][0] == 0) firstColHasZero = true;
-        }
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
 
-        // Use first row and column as markers
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                if (matrix[i][j] == 0) {
-                    matrix[i][0] = 0;  // mark row
-                    matrix[0][j] = 0;  // mark column
-                }
+            int midVal = matrix[mid / n][mid % n];
+
+            if (midVal == target) {
+                return true;
+            } else if (midVal < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
 
-        // Set zeros based on markers
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-
-        // Set first row to zero if needed
-        if (firstRowHasZero) {
-            for (int j = 0; j < n; j++) matrix[0][j] = 0;
-        }
-
-        // Set first column to zero if needed
-        if (firstColHasZero) {
-            for (int i = 0; i < m; i++) matrix[i][0] = 0;
-        }
+        return false;
     }
 }
