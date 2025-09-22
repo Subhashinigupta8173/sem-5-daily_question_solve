4d06f4c1d8aade52ce285fbf36fba2be346c58b8
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2025-09-22 21:03:43 +0530

    File updated at 22/9/2025, 9:03:42 pm

--- 63-UniquePathsII/63-UniquePathsII.java
+++ 63-UniquePathsII/63-UniquePathsII.java
@@ -1,23 +1,33 @@
-// Last updated: 22/9/2025, 9:02:18 pm
-class Solution {
-    public List<List<Integer>> subsetsWithDup(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        Arrays.sort(nums); // Step 1: sort to handle duplicates
-        backtrack(nums, 0, new ArrayList<>(), result);
-        return result;
+// Last updated: 22/9/2025, 9:03:42 pm
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+ class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return helper(nums, 0, nums.length - 1);
     }
 
-    private void backtrack(int[] nums, int start, List<Integer> tempList, List<List<Integer>> result) {
-        result.add(new ArrayList<>(tempList)); // add current subset
+    private TreeNode helper(int[] nums, int start, int end) {
+        if (start > end) return null;
 
-        for (int i = start; i < nums.length; i++) {
-            // skip duplicates
-            if (i > start && nums[i] == nums[i - 1]) continue;
+        int mid = start + (end - start) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
 
-            tempList.add(nums[i]);             // choose
-            backtrack(nums, i + 1, tempList, result); // explore
-            tempList.remove(tempList.size() - 1);     // un-choose (backtrack)
-        }
+        root.left = helper(nums, start, mid - 1);
+        root.right = helper(nums, mid + 1, end);
+
+        return root;
     }
 }
-
