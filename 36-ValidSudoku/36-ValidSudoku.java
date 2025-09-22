7cfb6ef80ec433784e63218d0999bac4054884b8
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2025-09-22 20:15:06 +0530

    File updated at 22/9/2025, 8:15:05 pm

--- 36-ValidSudoku/36-ValidSudoku.java
+++ 36-ValidSudoku/36-ValidSudoku.java
@@ -1,47 +1,32 @@
-// Last updated: 22/9/2025, 8:08:18 pm
+// Last updated: 22/9/2025, 8:15:05 pm
 class Solution {
-    public void solveSudoku(char[][] grid) {
-        Print(grid, 0, 0);
+    public String countAndSay(int n) {
+
+        String result = "1";
+
+        
+        for (int i = 2; i <= n; i++) {
+            result = buildNext(result);
+        }
+
+        return result;
     }
 
-    public static boolean Print(char[][] grid, int row, int col) {
-        if (col == 9) {
-            row++;
-            col = 0;
-        }
-        if (row == 9) return true;
-
-        if (grid[row][col] != '.') { // skip filled cells
-            return Print(grid, row, col + 1);
-        } else {
-            for (char val = '1'; val <= '9'; val++) { // try '1' to '9'
-                if (isitsafe(grid, row, col, val)) {
-                    grid[row][col] = val;
-                    if (Print(grid, row, col + 1)) return true;
-                    grid[row][col] = '.'; // backtrack
-                }
+   
+    private String buildNext(String s) {
+        StringBuilder sb = new StringBuilder();
+        int count = 1;  
+        for (int i = 1; i <= s.length(); i++) {
+        
+            if (i < s.length() && s.charAt(i) == s.charAt(i - 1)) {
+                count++;
+            } else {
+               
+                sb.append(count).append(s.charAt(i - 1));
+                count = 1;  
             }
         }
-        return false; // no number fits
-    }
 
-    public static boolean isitsafe(char[][] grid, int row, int col, char val) {
-        // check row
-        for (int i = 0; i < 9; i++) {
-            if (grid[row][i] == val) return false;
-        }
-        // check column
-        for (int i = 0; i < 9; i++) {
-            if (grid[i][col] == val) return false;
-        }
-        // check 3x3 box
-        int r = row - row % 3;
-        int c = col - col % 3;
-        for (int i = r; i < r + 3; i++) {
-            for (int j = c; j < c + 3; j++) {
-                if (grid[i][j] == val) return false;
-            }
-        }
-        return true;
+        return sb.toString();
     }
 }
