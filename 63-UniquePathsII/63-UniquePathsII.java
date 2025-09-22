cd37d81196a1ee1aedd76db38f15f74ce2938f2a
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2025-09-22 20:57:08 +0530

    File updated at 22/9/2025, 8:57:07 pm

--- 63-UniquePathsII/63-UniquePathsII.java
+++ 63-UniquePathsII/63-UniquePathsII.java
@@ -1,41 +1,47 @@
-// Last updated: 22/9/2025, 8:52:20 pm
+// Last updated: 22/9/2025, 8:57:07 pm
 class Solution {
-    public int[][] generateMatrix(int n) {
-        int[][] matrix = new int[n][n];
-        int top = 0, bottom = n - 1;
-        int left = 0, right = n - 1;
-        int num = 1;
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        boolean firstRowHasZero = false;
+        boolean firstColHasZero = false;
 
-        while (num <= n * n) {
-            // Move right
-            for (int i = left; i <= right; i++) {
-                matrix[top][i] = num++;
-            }
-            top++;
+        // Check if first row has zero
+        for (int j = 0; j < n; j++) {
+            if (matrix[0][j] == 0) firstRowHasZero = true;
+        }
 
-            // Move down
-            for (int i = top; i <= bottom; i++) {
-                matrix[i][right] = num++;
-            }
-            right--;
+        // Check if first column has zero
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) firstColHasZero = true;
+        }
 
-            // Move left
-            if (top <= bottom) {
-                for (int i = right; i >= left; i--) {
-                    matrix[bottom][i] = num++;
+        // Use first row and column as markers
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;  // mark row
+                    matrix[0][j] = 0;  // mark column
                 }
-                bottom--;
-            }
-
-            // Move up
-            if (left <= right) {
-                for (int i = bottom; i >= top; i--) {
-                    matrix[i][left] = num++;
-                }
-                left++;
             }
         }
 
-        return matrix;
+        // Set zeros based on markers
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Set first row to zero if needed
+        if (firstRowHasZero) {
+            for (int j = 0; j < n; j++) matrix[0][j] = 0;
+        }
+
+        // Set first column to zero if needed
+        if (firstColHasZero) {
+            for (int i = 0; i < m; i++) matrix[i][0] = 0;
+        }
     }
 }
