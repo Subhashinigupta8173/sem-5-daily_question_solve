8bd7a337b7b18a24ebe1b871c83707bec918ed1a
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2025-09-22 21:04:38 +0530

    File updated at 22/9/2025, 9:04:37 pm

--- 63-UniquePathsII/63-UniquePathsII.java
+++ 63-UniquePathsII/63-UniquePathsII.java
@@ -1,33 +1,17 @@
-// Last updated: 22/9/2025, 9:03:42 pm
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
- class Solution {
-    public TreeNode sortedArrayToBST(int[] nums) {
-        return helper(nums, 0, nums.length - 1);
-    }
+// Last updated: 22/9/2025, 9:04:37 pm
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> row = new ArrayList<>();
+        row.add(1); // row 0
 
-    private TreeNode helper(int[] nums, int start, int end) {
-        if (start > end) return null;
+        for (int i = 1; i <= rowIndex; i++) {
+            // Update from right to left
+            for (int j = row.size() - 1; j >= 1; j--) {
+                row.set(j, row.get(j) + row.get(j - 1));
+            }
+            row.add(1); // last element
+        }
 
-        int mid = start + (end - start) / 2;
-        TreeNode root = new TreeNode(nums[mid]);
-
-        root.left = helper(nums, start, mid - 1);
-        root.right = helper(nums, mid + 1, end);
-
-        return root;
+        return row;
     }
 }
