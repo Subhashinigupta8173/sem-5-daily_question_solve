24af1e3e6ed6d9e7fb49cb4a60fdd84b7340662a
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2025-09-23 23:49:39 +0530

    File updated at 23/9/2025, 11:49:40 pm

--- 8-StringtoInteger(atoi)/8-StringtoInteger(atoi).java
+++ 8-StringtoInteger(atoi)/8-StringtoInteger(atoi).java
@@ -1,34 +1,24 @@
-// Last updated: 23/9/2025, 11:48:33 pm
-import java.util.*;
+// Last updated: 23/9/2025, 11:49:40 pm
 
 class Solution {
-    public String getPermutation(int n, int k) {
-        // List of numbers to get elements from
-        List<Integer> numbers = new ArrayList<>();
-        for (int i = 1; i <= n; i++) {
-            numbers.add(i);
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(1, n, k, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void backtrack(int start, int n, int k, List<Integer> temp, List<List<Integer>> result) {
+        // If combination size reaches k, add it to result
+        if (temp.size() == k) {
+            result.add(new ArrayList<>(temp));
+            return;
         }
-        
-        // Factorials up to n
-        int[] factorial = new int[n];
-        factorial[0] = 1;
-        for (int i = 1; i < n; i++) {
-            factorial[i] = factorial[i - 1] * i;
+
+        // Try each number from start to n
+        for (int i = start; i <= n; i++) {
+            temp.add(i);                    // Choose number i
+            backtrack(i + 1, n, k, temp, result); // Recurse
+            temp.remove(temp.size() - 1);   // Backtrack
         }
-        
-        // Convert k to 0-based index
-        k--;
-        
-        StringBuilder sb = new StringBuilder();
-        
-        for (int i = n; i >= 1; i--) {
-            int index = k / factorial[i - 1];
-            sb.append(numbers.get(index));
-            numbers.remove(index);
-            
-            k = k % factorial[i - 1];
-        }
-        
-        return sb.toString();
     }
 }
