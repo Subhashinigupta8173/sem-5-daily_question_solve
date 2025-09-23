3a836cd27dc14ddc8e5fa06e276b9ba0dcdbafbc
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2025-09-23 23:44:38 +0530

    File updated at 23/9/2025, 11:44:38 pm

--- 8-StringtoInteger(atoi)/8-StringtoInteger(atoi).java
+++ 8-StringtoInteger(atoi)/8-StringtoInteger(atoi).java
@@ -1,31 +1,44 @@
-// Last updated: 23/9/2025, 11:43:33 pm
+// Last updated: 23/9/2025, 11:44:38 pm
 class Solution {
-    public int myAtoi(String s) {
-        if (s == null || s.length() == 0) return 0;
-
-        int i = 0, n = s.length();
-        // 1. Skip leading whitespaces
-        while (i < n && s.charAt(i) == ' ') i++;
-
-        // 2. Handle sign
-        int sign = 1;
-        if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
-            if (s.charAt(i) == '-') sign = -1;
-            i++;
+    public boolean isMatch(String s, String p) {
+        int m = s.length();
+        int n = p.length();
+        
+        // dp[i][j] = true if first i chars of s match first j chars of p
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        
+        // Empty string matches empty pattern
+        dp[0][0] = true;
+        
+        // Fill first row (s is empty)
+        for (int j = 1; j <= n; j++) {
+            if (p.charAt(j - 1) == '*') {
+                // '*' can eliminate the preceding character
+                dp[0][j] = dp[0][j - 2];
+            }
         }
-
-        // 3. Convert digits to integer
-        long result = 0; // use long to prevent overflow during calculation
-        while (i < n && Character.isDigit(s.charAt(i))) {
-            result = result * 10 + (s.charAt(i) - '0');
-
-            // 4. Check overflow
-            if (sign * result > Integer.MAX_VALUE) return Integer.MAX_VALUE;
-            if (sign * result < Integer.MIN_VALUE) return Integer.MIN_VALUE;
-
-            i++;
+        
+        // Fill the DP table
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                char sc = s.charAt(i - 1);
+                char pc = p.charAt(j - 1);
+                
+                if (pc == sc || pc == '.') {
+                    dp[i][j] = dp[i - 1][j - 1]; // Match current char
+                } else if (pc == '*') {
+                    char prev = p.charAt(j - 2);
+                    // Two possibilities:
+                    // 1. Treat '*' as zero occurrence of previous char
+                    dp[i][j] = dp[i][j - 2];
+                    // 2. Treat '*' as multiple occurrences if previous char matches
+                    if (prev == sc || prev == '.') {
+                        dp[i][j] = dp[i][j] || dp[i - 1][j];
+                    }
+                }
+            }
         }
-
-        return (int)(sign * result);
+        
+        return dp[m][n];
     }
 }
