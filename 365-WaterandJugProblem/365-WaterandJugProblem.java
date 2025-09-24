d97d74c224f2e755d088fc6b780e482abd00f0a0
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2025-09-24 20:34:21 +0530

    File updated at 24/9/2025, 8:34:20 pm

--- 365-WaterandJugProblem/365-WaterandJugProblem.java
+++ 365-WaterandJugProblem/365-WaterandJugProblem.java
@@ -1,37 +1,23 @@
-// Last updated: 24/9/2025, 8:31:54 pm
+// Last updated: 24/9/2025, 8:34:20 pm
 class Solution {
-    public List<Integer> largestDivisibleSubset(int[] nums) {
-        Arrays.sort(nums);
-        int n = nums.length;
-        
-        int[] dp = new int[n];       // dp[i] = length of largest subset ending at i
-        int[] prev = new int[n];     // prev[i] = previous index in the chain
-        Arrays.fill(dp, 1);
-        Arrays.fill(prev, -1);
-        
-        int maxLen = 1, maxIndex = 0;
-        
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[i] % nums[j] == 0 && dp[j] + 1 > dp[i]) {
-                    dp[i] = dp[j] + 1;
-                    prev[i] = j;
+    public int getMoneyAmount(int n) {
+        int[][] dp = new int[n + 2][n + 2]; // dp[i][j] = min cost for range i..j
+
+        for (int len = 2; len <= n; len++) { // length of range
+            for (int start = 1; start <= n - len + 1; start++) {
+                int end = start + len - 1;
+                dp[start][end] = Integer.MAX_VALUE;
+
+                for (int x = start; x <= end; x++) {
+                    int cost = x + Math.max(
+                        x - 1 >= start ? dp[start][x - 1] : 0,
+                        x + 1 <= end ? dp[x + 1][end] : 0
+                    );
+                    dp[start][end] = Math.min(dp[start][end], cost);
                 }
             }
-            if (dp[i] > maxLen) {
-                maxLen = dp[i];
-                maxIndex = i;
-            }
         }
-        
-        // Reconstruct subset
-        List<Integer> result = new ArrayList<>();
-        while (maxIndex != -1) {
-            result.add(nums[maxIndex]);
-            maxIndex = prev[maxIndex];
-        }
-        Collections.reverse(result);
-        return result;
+        return dp[1][n];
         
     }
 }
