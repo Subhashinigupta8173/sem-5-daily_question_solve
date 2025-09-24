3b2effca063d7c954e3235688c204653d56dbb4d
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2025-09-24 20:31:54 +0530

    File updated at 24/9/2025, 8:31:54 pm

--- 365-WaterandJugProblem/365-WaterandJugProblem.java
+++ 365-WaterandJugProblem/365-WaterandJugProblem.java
@@ -1,14 +1,37 @@
-// Last updated: 24/9/2025, 8:31:07 pm
+// Last updated: 24/9/2025, 8:31:54 pm
 class Solution {
-    public boolean canMeasureWater(int x, int y, int target) {
-        if (target > x + y) return false;
-        if (target == x || target == y || target == x + y) return true;
-        return target % gcd(x, y) == 0;
-    }
-
-    private int gcd(int a, int b) {
-        if (b == 0) return a;
-        return gcd(b, a % b);
+    public List<Integer> largestDivisibleSubset(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        
+        int[] dp = new int[n];       // dp[i] = length of largest subset ending at i
+        int[] prev = new int[n];     // prev[i] = previous index in the chain
+        Arrays.fill(dp, 1);
+        Arrays.fill(prev, -1);
+        
+        int maxLen = 1, maxIndex = 0;
+        
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] % nums[j] == 0 && dp[j] + 1 > dp[i]) {
+                    dp[i] = dp[j] + 1;
+                    prev[i] = j;
+                }
+            }
+            if (dp[i] > maxLen) {
+                maxLen = dp[i];
+                maxIndex = i;
+            }
+        }
+        
+        // Reconstruct subset
+        List<Integer> result = new ArrayList<>();
+        while (maxIndex != -1) {
+            result.add(nums[maxIndex]);
+            maxIndex = prev[maxIndex];
+        }
+        Collections.reverse(result);
+        return result;
         
     }
 }
