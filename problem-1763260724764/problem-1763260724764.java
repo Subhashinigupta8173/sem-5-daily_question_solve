dc7268ec83d5ce1ef77de2cae8864f7a01dfbedc
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2025-11-16 08:08:44 +0530

    Create

---
+++ problem-1763260724764/problem-1763260724764.java
@@ -0,0 +1,9 @@
+// Last updated: 16/11/2025, 8:08:44 am
+class Solution {
+    public int maximizeExpressionOfThree(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length-1]+nums[nums.length-2]-nums[0];
+       
+        
+    }
+}
