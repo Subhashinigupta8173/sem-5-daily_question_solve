630c5c2c7664fbf925108e3cf756b3e729fae29e
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2025-11-26 00:15:46 +0530

    File updated at 26/11/2025, 12:15:46 am

--- 189-RotateArray/189-RotateArray.java
+++ 189-RotateArray/189-RotateArray.java
@@ -1,44 +1,24 @@
-// Last updated: 5/8/2025, 10:25:05 pm
+// Last updated: 26/11/2025, 12:15:46 am
 class Solution {
     public void rotate(int[] nums, int k) {
-        k = k % nums.length; 
-        reverse(nums, 0, nums.length - 1);
-        reverse(nums, 0, k - 1);
-        reverse(nums, k, nums.length - 1);
+        int n=nums.length;
+        k=k%n;
+       
+        reverse(nums,0,n-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,n-1);
+
+
+        
     }
 
-    private void reverse(int[] nums, int start, int end) {
-        while (start < end) {
-            int temp = nums[start];
-            nums[start] = nums[end];
-            nums[end] = temp;
-            start++;
-            end--;
+     public static void reverse(int[] nums,int i,int j){
+    while(i<j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+        i++;
+        j--;
         }
-    }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println();
-        int n = scanner.nextInt();
-
-        if (n <= 0) {
-            System.out.println();
-            return;
-        }
-
-        int[] nums = new int[n];
-
-        System.out.println();
-        for (int i = 0; i < n; i++) {
-            nums[i] = scanner.nextInt();
-        }
-        int k = scanner.nextInt();
-
-        Solution solution = new Solution();
-        solution.rotate(nums, k);
-
-        System.out.println();
-        System.out.println(Arrays.toString(nums));
     }
 }
