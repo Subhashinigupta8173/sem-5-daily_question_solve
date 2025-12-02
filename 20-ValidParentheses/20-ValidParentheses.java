45796ed72b992f87438896ad57e4ea8f1ddc9da6
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2025-12-02 23:08:03 +0530

    File updated at 2/12/2025, 11:08:02 pm

--- 20-ValidParentheses/20-ValidParentheses.java
+++ 20-ValidParentheses/20-ValidParentheses.java
@@ -1,26 +1,27 @@
-// Last updated: 5/8/2025, 10:25:56 pm
-class Solution {
-    public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        
-        for (char c : s.toCharArray()) {
-            
-            if (c == '(' || c == '{' || c == '[') {
-                stack.push(c);
-            } else {
-        
-                if (stack.isEmpty() || !isMatch(stack.pop(), c)) {
-                    return false;
-                }
-            }
-        }
-      
-        return stack.isEmpty();
-    }
-    
-  
-    public static boolean isMatch(char open, char close) {
-        return (open == '(' && close == ')') ||(open == '{' && close == '}') ||
-               (open == '[' && close == ']');
-    }
-}
+// Last updated: 2/12/2025, 11:08:02 pm
+1class Solution {
+2    public boolean isValid(String s) {
+3         Stack<Character> st = new Stack<>();
+4        
+5        for (char ch : s.toCharArray()) {
+6            // Push opening brackets
+7            if (ch == '(' || ch == '{' || ch == '[') {
+8                st.push(ch);
+9            } else {
+10                // If stack empty and closing bracket appears -> NOT valid
+11                if (st.isEmpty()) return false;
+12                
+13                char top = st.pop();
+14                
+15                // Check mismatch
+16                if (ch == ')' && top != '(') return false;
+17                if (ch == '}' && top != '{') return false;
+18                if (ch == ']' && top != '[') return false;
+19            }
+20        }
+21        
+22        // At end stack must be empty
+23        return st.isEmpty();
+24        
+25    }
+26}
