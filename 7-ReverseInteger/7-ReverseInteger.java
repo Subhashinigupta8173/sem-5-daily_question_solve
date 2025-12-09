ae71b4c6821da4956e9de091d1233bca160ec0b5
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2025-12-09 22:25:07 +0530

    File updated at 9/12/2025, 10:25:07 pm

--- 7-ReverseInteger/7-ReverseInteger.java
+++ 7-ReverseInteger/7-ReverseInteger.java
@@ -1,21 +1,33 @@
-// Last updated: 14/8/2025, 11:14:20 pm
-class Solution {
-    public int reverse(int x) {
-        int rev = 0;
-        while (x != 0) {
-            int pop = x % 10;   
-            x /= 10;            
-
-          
-            if (rev > Integer.MAX_VALUE/10 || 
-               (rev == Integer.MAX_VALUE/10 && pop > 7)) return 0;
-
-           
-            if (rev < Integer.MIN_VALUE/10 || 
-               (rev == Integer.MIN_VALUE/10 && pop < -8)) return 0;
-
-            rev = rev * 10 + pop;
-        }
-        return rev;
-    }
-}
+// Last updated: 9/12/2025, 10:25:07 pm
+1class Solution {
+2    public boolean isMatch(String s, String p) {
+3        int i = 0, j = 0, starIndex = -1, matchIndex = 0;
+4
+5        while (i < s.length()) {
+6            if (j < p.length() && (p.charAt(j) == '?' || p.charAt(j) == s.charAt(i))) {
+7                i++;
+8                j++;
+9            } 
+10            else if (j < p.length() && p.charAt(j) == '*') {
+11                starIndex = j;
+12                matchIndex = i;
+13                j++;
+14            } 
+15            else if (starIndex != -1) {
+16                j = starIndex + 1;
+17                matchIndex++;
+18                i = matchIndex;
+19            } 
+20            else {
+21                return false;
+22            }
+23        }
+24
+25        while (j < p.length() && p.charAt(j) == '*') {
+26            j++;
+27        }
+28
+29        return j == p.length();
+30    }
+31}
+32
