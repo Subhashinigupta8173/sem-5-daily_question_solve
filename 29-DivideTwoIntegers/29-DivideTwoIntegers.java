f0fff56600e661c71c429cc3c5aa4b749d8278a2
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2026-01-03 21:53:17 +0530

    File updated at 3/1/2026, 9:53:17 pm

--- 29-DivideTwoIntegers/29-DivideTwoIntegers.java
+++ 29-DivideTwoIntegers/29-DivideTwoIntegers.java
@@ -1,31 +1,30 @@
-// Last updated: 22/9/2025, 9:24:23 pm
-class Solution {
-    public int divide(int dividend, int divisor) {
-        // Edge case: overflow
-        if (dividend == Integer.MIN_VALUE && divisor == -1) 
-            return Integer.MAX_VALUE;
-
-        // Determine the sign of the result
-        boolean negative = (dividend < 0) ^ (divisor < 0);
-
-        // Convert to negatives to handle overflow
-        int dvd = dividend > 0 ? -dividend : dividend;
-        int dvs = divisor > 0 ? -divisor : divisor;
-
-        int quotient = 0;
-
-        while (dvd <= dvs) {
-            int temp = dvs;
-            int multiple = 1;
-            // Double temp until it can't be subtracted
-            while (temp >= Integer.MIN_VALUE >> 1 && dvd <= (temp << 1)) {
-                temp <<= 1;
-                multiple <<= 1;
-            }
-            dvd -= temp;
-            quotient += multiple;
-        }
-
-        return negative ? -quotient : quotient;
-    }
-}
+// Last updated: 3/1/2026, 9:53:17 pm
+1class Solution {
+2    public int firstMissingPositive(int[] nums) {
+3        int n = nums.length;
+4
+5      
+6        for (int i = 0; i < n; i++) {
+7            while (
+8                nums[i] > 0 &&
+9                nums[i] <= n &&
+10                nums[nums[i] - 1] != nums[i]
+11            ) {
+12                int temp = nums[i];
+13                nums[i] = nums[temp - 1];
+14                nums[temp - 1] = temp;
+15            }
+16        }
+17
+18       
+19        for (int i = 0; i < n; i++) {
+20            if (nums[i] != i + 1) {
+21                return i + 1;
+22            }
+23        }
+24
+25       
+26        return n + 1;
+27    }
+28}
+29
