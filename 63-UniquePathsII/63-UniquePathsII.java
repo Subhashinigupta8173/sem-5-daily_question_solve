3dbf2dc00eab67c3e071b3aa0b156d07702c7312
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2026-01-09 14:04:01 +0530

    File updated at 9/1/2026, 2:04:01 pm

--- 63-UniquePathsII/63-UniquePathsII.java
+++ 63-UniquePathsII/63-UniquePathsII.java
@@ -1,21 +1,26 @@
-// Last updated: 22/9/2025, 9:06:41 pm
-class Solution {
-    public int minimumTotal(List<List<Integer>> triangle) {
-        int n = triangle.size();
-        int[] dp = new int[n];
-
-        // initialize dp with last row
-        for (int j = 0; j < n; j++) {
-            dp[j] = triangle.get(n - 1).get(j);
-        }
-
-        // start from second last row
-        for (int i = n - 2; i >= 0; i--) {
-            for (int j = 0; j <= i; j++) {
-                dp[j] = triangle.get(i).get(j) + Math.min(dp[j], dp[j + 1]);
-            }
-        }
-
-        return dp[0];
-    }
-}
+// Last updated: 9/1/2026, 2:04:01 pm
+1class Solution {
+2    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+3         int m = obstacleGrid.length;
+4        int n = obstacleGrid[0].length;
+5        int[][] dp = new int[m][n];
+6
+7        for (int i = 0; i < m; i++) {
+8            for (int j = 0; j < n; j++) {
+9                if (obstacleGrid[i][j] == 1) {
+10                    dp[i][j] = 0; // obstacle
+11                } else if (i == 0 && j == 0) {
+12                    dp[i][j] = 1; // start
+13                } else {
+14                    int up = (i > 0) ? dp[i - 1][j] : 0;
+15                    int left = (j > 0) ? dp[i][j - 1] : 0;
+16                    dp[i][j] = up + left;
+17                }
+18            }
+19        }
+20
+21        return dp[m - 1][n - 1];
+22    }
+23        
+24    
+25}
