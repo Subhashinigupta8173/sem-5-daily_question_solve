36e6ec65225194ff95f6d77aa9526f1e24ae9d66
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2026-01-19 21:54:31 +0530

    File updated at 19/1/2026, 9:54:31 pm

--- 2-AddTwoNumbers/2-AddTwoNumbers.java
+++ 2-AddTwoNumbers/2-AddTwoNumbers.java
@@ -1,35 +1,35 @@
-// Last updated: 5/8/2025, 10:26:09 pm
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummyHead=new ListNode(0);
-        ListNode current = dummyHead;
-        int carry =0;
-  
-    
-    while(l1!=null || l2!= null){
-        int x=(l1!=null)? l1.val:0;
-        int y=(l2!=null )?l2.val:0;
-        int sum= carry+x+y;
-        carry =sum/10;
-        current.next=new ListNode(sum%10);
-        current=current.next;
-        if(l1!=null) l1=l1.next;
-        if(l2!=null)l2=l2.next;
-    }
-    if(carry>0){
-        current.next=new ListNode(carry);
-
-    }
-    return dummyHead.next;
-    }
-}
+// Last updated: 19/1/2026, 9:54:31 pm
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11class Solution {
+12    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+13        ListNode dummyHead=new ListNode(0);
+14        ListNode current = dummyHead;
+15        int carry =0;
+16  
+17    
+18    while(l1!=null || l2!= null){
+19        int x=(l1!=null)? l1.val:0;
+20        int y=(l2!=null )?l2.val:0;
+21        int sum= carry+x+y;
+22        carry =sum/10;
+23        current.next=new ListNode(sum%10);
+24        current=current.next;
+25        if(l1!=null) l1=l1.next;
+26        if(l2!=null)l2=l2.next;
+27    }
+28    if(carry>0){
+29        current.next=new ListNode(carry);
+30
+31    }
+32    return dummyHead.next;
+33    }
+34}
