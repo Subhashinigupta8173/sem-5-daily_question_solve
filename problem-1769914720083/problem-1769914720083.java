7a245d2f6d9680031014dda36139e172c517e3c6
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2026-02-01 08:28:40 +0530

    Create

---
+++ problem-1769914720083/problem-1769914720083.java
@@ -0,0 +1,14 @@
+// Last updated: 1/2/2026, 8:28:40 am
+1class Solution {
+2    public int finalElement(int[] nums) {
+3       
+4       
+5        int r=nums.length;
+6        return Math.max(nums[0],nums[r-1]);
+7       
+8        
+9        
+10        
+11    }
+12   
+13}
