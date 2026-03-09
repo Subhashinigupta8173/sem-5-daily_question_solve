7470328fd20fcb6484662b52771a2af369dbe6f1
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2026-03-09 18:00:38 +0530

    File updated at 9/3/2026, 6:00:37 pm

--- 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.java
+++ 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.java
@@ -1,18 +1,19 @@
-// Last updated: 9/3/2026, 5:54:42 pm
-1
-2class Solution {
-3    public int removeDuplicates(int[] nums) {
-4        
-5        int k = 1;   
-6        
-7        for(int i = 1; i < nums.length; i++){
-8            
-9            if(nums[i] != nums[i-1]){
-10                nums[k] = nums[i];
-11                k++;
-12            }
-13        }
-14        
-15        return k;
-16    }
-17}
+// Last updated: 9/3/2026, 6:00:37 pm
+1class Solution {
+2    public int reverse(int x) {
+3        
+4        int rev = 0;
+5        
+6        while(x != 0){
+7            int digit = x % 10;
+8            x = x / 10;
+9            
+10            if(rev > Integer.MAX_VALUE/10 || rev < Integer.MIN_VALUE/10)
+11                return 0;
+12                
+13            rev = rev * 10 + digit;
+14        }
+15        
+16        return rev;
+17    }
+18}
