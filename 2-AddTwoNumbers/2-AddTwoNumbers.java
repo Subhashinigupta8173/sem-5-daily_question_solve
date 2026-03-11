83d94caec18b31326f50426a749c4bad310302bc
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2026-03-11 18:40:16 +0530

    File updated at 11/3/2026, 6:40:16 pm

--- 2-AddTwoNumbers/2-AddTwoNumbers.java
+++ 2-AddTwoNumbers/2-AddTwoNumbers.java
@@ -1,29 +1,36 @@
-// Last updated: 11/3/2026, 6:02:25 pm
-1import java.util.*;
-2
-3class Solution {
-4    public int lengthOfLongestSubstring(String s) {
+// Last updated: 11/3/2026, 6:40:16 pm
+1class Solution {
+2    public String longestPalindrome(String s) {
+3
+4        String ans = "";
 5
-6        HashMap<Character,Integer> freq = new HashMap<>();
+6        for(int i = 0; i < s.length(); i++) {
 7
-8        int j = 0;
-9        int max = 0;
+8           
+9            String s1 = expand(s, i, i);
 10
-11        for(int i = 0; i < s.length(); i++){
-12
-13            char c = s.charAt(i);
-14
-15            freq.put(c, freq.getOrDefault(c,0) + 1);
+11           
+12            String s2 = expand(s, i, i + 1);
+13
+14            if(s1.length() > ans.length())
+15                ans = s1;
 16
-17            while(freq.get(c) > 1){
-18                char left = s.charAt(j);
-19                freq.put(left, freq.get(left) - 1);
-20                j++;
-21            }
-22
-23            max = Math.max(max, i - j + 1);
-24        }
+17            if(s2.length() > ans.length())
+18                ans = s2;
+19        }
+20
+21        return ans;
+22    }
+23
+24    public String expand(String s, int left, int right){
 25
-26        return max;
-27    }
-28}
+26        while(left >= 0 && right < s.length() &&
+27              s.charAt(left) == s.charAt(right)){
+28
+29            left--;
+30            right++;
+31        }
+32
+33        return s.substring(left + 1, right);
+34    }
+35}
