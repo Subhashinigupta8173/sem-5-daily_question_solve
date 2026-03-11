1043811ae66ff388a8f2bb0bf1f76b15cbb822eb
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2026-03-11 19:06:20 +0530

    File updated at 11/3/2026, 7:06:21 pm

--- 8-StringtoInteger(atoi)/8-StringtoInteger(atoi).java
+++ 8-StringtoInteger(atoi)/8-StringtoInteger(atoi).java
@@ -1,32 +1,16 @@
-// Last updated: 19/1/2026, 9:50:17 pm
+// Last updated: 11/3/2026, 7:06:21 pm
 1class Solution {
-2    public int myAtoi(String s) {
-3        if (s == null || s.length() == 0) return 0;
-4
-5        int i = 0, n = s.length();
-6        // 1. Skip leading whitespaces
-7        while (i < n && s.charAt(i) == ' ') i++;
-8
-9        // 2. Handle sign
-10        int sign = 1;
-11        if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
-12            if (s.charAt(i) == '-') sign = -1;
-13            i++;
-14        }
-15
-16        // 3. Convert digits to integer
-17        long result = 0; // use long to prevent overflow during calculation
-18        while (i < n && Character.isDigit(s.charAt(i))) {
-19            result = result * 10 + (s.charAt(i) - '0');
-20
-21            // 4. Check overflow
-22            if (sign * result > Integer.MAX_VALUE) return Integer.MAX_VALUE;
-23            if (sign * result < Integer.MIN_VALUE) return Integer.MIN_VALUE;
-24
-25            i++;
-26        }
-27
-28        return (int)(sign * result);
-29    }
-30}
-31
+2    public boolean isPalindrome(int x) {
+3        int ans=x;
+4        int rev=0;
+5        if(x<0){
+6            return false;
+7        }
+8        while(x!=0){
+9            int d=x%10;
+10            rev=rev*10+d;
+11            x=x/10;
+12        }
+13        return rev==ans;
+14    }
+15}
