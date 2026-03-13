103a1eb123ea9480c41dc925be62bb63c7d74392
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2026-03-13 18:36:06 +0530

    File updated at 13/3/2026, 6:36:06 pm

--- 31-NextPermutation/31-NextPermutation.java
+++ 31-NextPermutation/31-NextPermutation.java
@@ -1,36 +1,32 @@
-// Last updated: 13/3/2026, 5:41:02 pm
+// Last updated: 13/3/2026, 6:36:06 pm
 1class Solution {
-2    public void nextPermutation(int[] nums) {
-3        int i=nums.length-2;
-4        //find pivot value means jo next greater se just small ai
-5        while(i>=0 && nums[i]>=nums[i+1]){
-6            i--;
-7        }
-8        // find next greater value to swap 
-9        if(i>=0){
-10        int j=nums.length-1;
-11        while( nums[j]<=nums[i]){
-12            j--;
-13        }
-14        //swap them
-15        int temp=nums[i];
-16        nums[i]=nums[j];
-17        nums[j]=temp;
-18        
-19    }
-20    // reverse whole array after the i
-21    reverse(nums,i+1);
-22    }
-23      private void reverse(int[] nums, int start) {
-24        int end = nums.length - 1;
-25
-26        while (start < end) {
-27            int temp = nums[start];
-28            nums[start] = nums[end];
-29            nums[end] = temp;
-30
-31            start++;
-32            end--;
-33        }
-34     }
-35}
+2    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+3        List<List<Integer>> result = new ArrayList<>();
+4         Arrays.sort(candidates);
+5        backtrack(candidates, target, 0, new ArrayList<>(), result);
+6        return result;
+7    }
+8
+9    private void backtrack(int[] candidates, int target, int start,
+10                           List<Integer> current, List<List<Integer>> result) {
+11
+12        if (target == 0) {
+13            result.add(new ArrayList<>(current));
+14            return;
+15        }
+16
+17        
+18        for (int i = start; i < candidates.length; i++) {
+19
+20            if(i > start && candidates[i] == candidates[i-1])
+21                continue;
+22
+23            if(candidates[i] > target)
+24                break;
+25            current.add(candidates[i]); // choose
+26            backtrack(candidates, target - candidates[i], i+1, current, result);
+27            current.remove(current.size() - 1); // backtrack
+28        }
+29        
+30    }
+31}
