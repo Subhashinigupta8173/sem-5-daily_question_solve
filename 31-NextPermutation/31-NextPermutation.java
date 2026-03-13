e12ef2b4c180c573788753a49797671e1a0216be
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2026-03-13 18:48:48 +0530

    File updated at 13/3/2026, 6:48:48 pm

--- 31-NextPermutation/31-NextPermutation.java
+++ 31-NextPermutation/31-NextPermutation.java
@@ -1,32 +1,23 @@
-// Last updated: 13/3/2026, 6:36:06 pm
+// Last updated: 13/3/2026, 6:48:48 pm
 1class Solution {
-2    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-3        List<List<Integer>> result = new ArrayList<>();
-4         Arrays.sort(candidates);
-5        backtrack(candidates, target, 0, new ArrayList<>(), result);
-6        return result;
-7    }
-8
-9    private void backtrack(int[] candidates, int target, int start,
-10                           List<Integer> current, List<List<Integer>> result) {
-11
-12        if (target == 0) {
-13            result.add(new ArrayList<>(current));
-14            return;
-15        }
-16
-17        
-18        for (int i = start; i < candidates.length; i++) {
-19
-20            if(i > start && candidates[i] == candidates[i-1])
-21                continue;
-22
-23            if(candidates[i] > target)
-24                break;
-25            current.add(candidates[i]); // choose
-26            backtrack(candidates, target - candidates[i], i+1, current, result);
-27            current.remove(current.size() - 1); // backtrack
-28        }
-29        
-30    }
-31}
+2    public List<List<Integer>> permute(int[] nums) {
+3        List<List<Integer>> result=new ArrayList<>();
+4        backtrack( nums,new ArrayList<>(),result);
+5        return result;
+6
+7        
+8    }
+9    private void backtrack(int[] nums,List<Integer> current,List<List<Integer>>result){
+10        if(current.size()==nums.length){
+11            result.add(new ArrayList<>(current));
+12        }
+13        for(int i=0; i<nums.length; i++){
+14            if(current.contains(nums[i])){
+15            continue;
+16            }
+17            current.add(nums[i]);
+18            backtrack(nums,current,result);
+19            current.remove(current.size()-1);
+20        }
+21    }
+22}
