9c38ad9781a10676ff242255d0e924633d2462de
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2026-03-13 19:07:10 +0530

    File updated at 13/3/2026, 7:07:09 pm

--- 31-NextPermutation/31-NextPermutation.java
+++ 31-NextPermutation/31-NextPermutation.java
@@ -1,32 +1,31 @@
-// Last updated: 13/3/2026, 6:54:14 pm
+// Last updated: 13/3/2026, 7:07:09 pm
 1class Solution {
-2    public List<List<Integer>> permuteUnique(int[] nums) {
-3        List<List<Integer>> result=new ArrayList<>();
-4        Arrays.sort(nums);
-5        boolean[] used = new boolean[nums.length];
-6        backtrack( nums,new ArrayList<>(),used,result);
-7        return result;
-8
-9        
-10    }
-11    private void backtrack(int[] nums,List<Integer> current,boolean [] used,List<List<Integer>>result){
-12        if(current.size()==nums.length){
-13            result.add(new ArrayList<>(current));
-14        }
-15        for(int i=0; i<nums.length; i++){
-16             if(used[i])
-17                continue;
+2    public void rotate(int[][] matrix) {
+3        int n=matrix.length;
+4        for(int i=0; i<n; i++){
+5            for(int j=i; j<n; j++){
+6                int temp = matrix[i][j];
+7                matrix[i][j] = matrix[j][i];
+8                matrix[j][i] = temp;
+9            }
+10        }
+11         
+12        for(int i = 0; i < n; i++){
+13
+14            int left = 0;
+15            int right = n - 1;
+16
+17            while(left < right){
 18
-19            if(i > 0 && nums[i] == nums[i-1] && !used[i-1])
-20                continue;
-21
-22            used[i] = true;
-23            
-24            current.add(nums[i]);
-25            backtrack(nums,current,used,result);
-26            current.remove(current.size()-1);
-27            used[i]=false;
-28        }
-29        
-30    }
-31}
+19                int temp = matrix[i][left];
+20                matrix[i][left] = matrix[i][right];
+21                matrix[i][right] = temp;
+22
+23                left++;
+24                right--;
+25            }
+26        }
+27
+28        
+29    }
+30}
