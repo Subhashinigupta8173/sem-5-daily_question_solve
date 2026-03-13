1ee6941de879703feef9c1fda86e62b707ea5bd0
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2026-03-13 17:41:02 +0530

    File updated at 13/3/2026, 5:41:02 pm

--- 31-NextPermutation/31-NextPermutation.java
+++ 31-NextPermutation/31-NextPermutation.java
@@ -1,72 +1,36 @@
-// Last updated: 5/8/2025, 10:25:47 pm
-class Solution {
-    public void nextPermutation(int[] nums) {
-        int k = -1;
-
-        
-        for (int i = nums.length - 2; i >= 0; i--) {
-            if (nums[i] < nums[i + 1]) {
-                k = i;
-                break;
-            }
-        }
-
-        
-        if (k == -1) {
-            reverse(nums, 0, nums.length - 1);
-            return;
-        }
-
-       
-        int l = -1;
-        for (int i = nums.length - 1; i > k; i--) {
-            if (nums[i] > nums[k]) {
-                l = i;
-                break;
-            }
-        }
-
-        
-        int temp = nums[k];
-        nums[k] = nums[l];
-        nums[l] = temp;
-
-        
-        reverse(nums, k + 1, nums.length - 1);
-    }
-
-    private void reverse(int[] nums, int start, int end) {
-        while (start < end) {
-            int temp = nums[start];
-            nums[start] = nums[end];
-            nums[end] = temp;
-            start++;
-            end--;
-        }
-    }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println();
-
-       
-        int n = scanner.nextInt();
-
-        if (n <= 0) {
-            System.out.println();
-            return;
-        }
-
-        int[] nums = new int[n];
-
-        System.out.println();
-        for (int i = 0; i < n; i++) {
-            nums[i] = scanner.nextInt();
-        }
-
-        Solution solution = new Solution();
-        solution.nextPermutation(nums);
-
-        System.out.println( Arrays.toString(nums));
-    }
-}
+// Last updated: 13/3/2026, 5:41:02 pm
+1class Solution {
+2    public void nextPermutation(int[] nums) {
+3        int i=nums.length-2;
+4        //find pivot value means jo next greater se just small ai
+5        while(i>=0 && nums[i]>=nums[i+1]){
+6            i--;
+7        }
+8        // find next greater value to swap 
+9        if(i>=0){
+10        int j=nums.length-1;
+11        while( nums[j]<=nums[i]){
+12            j--;
+13        }
+14        //swap them
+15        int temp=nums[i];
+16        nums[i]=nums[j];
+17        nums[j]=temp;
+18        
+19    }
+20    // reverse whole array after the i
+21    reverse(nums,i+1);
+22    }
+23      private void reverse(int[] nums, int start) {
+24        int end = nums.length - 1;
+25
+26        while (start < end) {
+27            int temp = nums[start];
+28            nums[start] = nums[end];
+29            nums[end] = temp;
+30
+31            start++;
+32            end--;
+33        }
+34     }
+35}
