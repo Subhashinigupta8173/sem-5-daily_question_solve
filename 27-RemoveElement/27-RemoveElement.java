eeb1c114a46b525f4de6bb30f2f701b157ef062d
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2026-03-14 17:39:51 +0530

    File updated at 14/3/2026, 5:39:51 pm

--- 27-RemoveElement/27-RemoveElement.java
+++ 27-RemoveElement/27-RemoveElement.java
@@ -1,23 +1,18 @@
-// Last updated: 14/3/2026, 5:22:55 pm
+// Last updated: 14/3/2026, 5:39:51 pm
 1class Solution {
-2    public int firstMissingPositive(int[] nums) {
-3
-4        int n = nums.length;
-5
-6        for(int i = 0; i < n; i++){
-7            while(nums[i] > 0 && nums[i] <= n && nums[nums[i]-1] != nums[i]){
-8                int temp = nums[i];
-9                nums[i] = nums[temp-1];
-10                nums[temp-1] = temp;
-11            }
+2    public int maxSubArray(int[] nums) {
+3        int sum=0;
+4        int max=Integer.MIN_VALUE;
+5        for(int i=0;i<nums.length;i++){
+6            sum+=nums[i];
+7            max=Math.max(max,sum);
+8            if(sum<0){
+9                sum=0;
+10            }
+11
 12        }
-13
-14        for(int i = 0; i < n; i++){
-15            if(nums[i] != i+1){
-16                return i+1;
-17            }
-18        }
-19
-20        return n+1;
-21    }
-22}
+13        return max;
+14
+15        
+16    }
+17}
