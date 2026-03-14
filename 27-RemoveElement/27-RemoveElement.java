83c028e22a60c76b923c3f23fd06731e5027a27c
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2026-03-14 15:33:44 +0530

    File updated at 14/3/2026, 3:33:44 pm

--- 27-RemoveElement/27-RemoveElement.java
+++ 27-RemoveElement/27-RemoveElement.java
@@ -1,27 +1,20 @@
-// Last updated: 5/8/2025, 10:25:49 pm
-class Solution {
-    public int removeElement(int[] nums, int val) {
-        int k = 0; // Index for the next position to place an element not equal to val
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != val) {
-                nums[k] = nums[i];
-                k++;
-            }
-        }
-        return k;
-    }
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-
-        int[] nums1 = {3, 2, 2, 3};
-        int val1 = 3;
-        int k1 = solution.removeElement(nums1, val1);
-        System.out.println("Output: " + k1 + ", nums = " + Arrays.toString(Arrays.copyOf(nums1, k1)));
-
-        int[] nums2 = {0, 1, 2, 2, 3, 0, 4, 2};
-        int val2 = 2;
-        int k2 = solution.removeElement(nums2, val2);
-        System.out.println("Output: " + k2 + ", nums = " + Arrays.toString(Arrays.copyOf(nums2, k2)));
-    }
-}
+// Last updated: 14/3/2026, 3:33:44 pm
+1class Solution {
+2    public int removeElement(int[] nums, int val) {
+3        
+4        int c=0;
+5        for(int i=0; i<nums.length; i++){
+6            
+7            if(nums[i]!=val){
+8                
+9                nums[c]=nums[i];
+10
+11               
+12
+13                c++;
+14            }
+15        }
+16        return c;
+17        
+18    }
+19}
