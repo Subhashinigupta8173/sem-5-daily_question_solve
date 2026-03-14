fc9fa008d87cb09465851ce9c1b6fa2a0a6efc62
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2026-03-14 17:50:41 +0530

    File updated at 14/3/2026, 5:50:42 pm

--- 27-RemoveElement/27-RemoveElement.java
+++ 27-RemoveElement/27-RemoveElement.java
@@ -1,18 +1,28 @@
-// Last updated: 14/3/2026, 5:39:51 pm
+// Last updated: 14/3/2026, 5:50:42 pm
 1class Solution {
-2    public int maxSubArray(int[] nums) {
-3        int sum=0;
-4        int max=Integer.MIN_VALUE;
-5        for(int i=0;i<nums.length;i++){
-6            sum+=nums[i];
-7            max=Math.max(max,sum);
-8            if(sum<0){
-9                sum=0;
-10            }
-11
+2    public int trap(int[] height) {
+3
+4        int n = height.length;
+5
+6        int[] leftMax = new int[n];
+7        int[] rightMax = new int[n];
+8
+9        leftMax[0] = height[0];
+10        for(int i = 1; i < n; i++){
+11            leftMax[i] = Math.max(leftMax[i-1], height[i]);
 12        }
-13        return max;
-14
-15        
-16    }
-17}
+13
+14        rightMax[n-1] = height[n-1];
+15        for(int i = n-2; i >= 0; i--){
+16            rightMax[i] = Math.max(rightMax[i+1], height[i]);
+17        }
+18
+19        int water = 0;
+20
+21        for(int i = 0; i < n; i++){
+22            water += Math.min(leftMax[i], rightMax[i]) - height[i];
+23        }
+24
+25        return water;
+26    }
+27}
