3f1f6a2468150aeed6a140f88a498d9bc765e25c
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2026-03-22 21:42:59 +0530

    File updated at 22/3/2026, 9:42:58 pm

--- 75-SortColors/75-SortColors.java
+++ 75-SortColors/75-SortColors.java
@@ -1,10 +1,8 @@
-// Last updated: 9/1/2026, 1:57:42 pm
+// Last updated: 22/3/2026, 9:42:58 pm
 1class Solution {
-2    public void sortColors(int[]arr) {
-3       
-4        Arrays.sort(arr);
+2    public void sortColors(int[] nums) {
+3        Arrays.sort(nums);
+4        
 5        
-6        
-7        
-8    }
-9}
+6    }
+7}
