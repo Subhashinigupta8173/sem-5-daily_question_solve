249343e72560a51d58258a2527e9d75374cf5d64
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2026-06-25 17:11:22 +0530

    File updated at 25/6/2026, 5:11:23 pm

--- 71-SimplifyPath/71-SimplifyPath.java
+++ 71-SimplifyPath/71-SimplifyPath.java
@@ -1,32 +1,35 @@
-// Last updated: 23/9/2025, 5:58:17 am
-class Solution {
-    public String simplifyPath(String path) {
-    
-        String[] parts = path.split("/");
-        Deque<String> stack = new ArrayDeque<>();
-        
-        for (String part : parts) {
-            if (part.equals("") || part.equals(".")) {
-                
-                continue;
-            } else if (part.equals("..")) {
-                
-                if (!stack.isEmpty()) {
-                    stack.pop();
-                }
-            } else {
-                
-                stack.push(part);
-            }
-        }
-        
-        
-        StringBuilder result = new StringBuilder();
-        for (String dir : stack) {
-            result.insert(0, "/" + dir);
-        }
-        
-        
-        return result.length() == 0 ? "/" : result.toString();
-    }
-}
+// Last updated: 25/6/2026, 5:11:23 pm
+1class Solution {
+2    public String simplifyPath(String path) {
+3
+4        Stack<String> st = new Stack<>();
+5        String[] parts = path.split("/");
+6
+7        for (String part : parts) {
+8
+9            if (part.equals("") || part.equals(".")) {
+10                continue;
+11            }
+12
+13            if (part.equals("..")) {
+14                if (!st.isEmpty()) {
+15                    st.pop();
+16                }
+17            } else {
+18                st.push(part);
+19            }
+20        }
+21
+22        if (st.isEmpty()) {
+23            return "/";
+24        }
+25
+26        StringBuilder ans = new StringBuilder();
+27
+28        for (String dir : st) {
+29            ans.append("/").append(dir);
+30        }
+31
+32        return ans.toString();
+33    }
+34}
