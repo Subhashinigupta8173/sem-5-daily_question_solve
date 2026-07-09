f672314a9d7a1e1f42a4c05f01029d06ac5d2487
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2026-07-09 10:42:11 +0530

    File updated at 8/7/2026, 10:42:52 pm

--- 532-K-diffPairsinanArray/532-K-diffPairsinanArray.java
+++ 532-K-diffPairsinanArray/532-K-diffPairsinanArray.java
@@ -1,30 +1,20 @@
-// Last updated: 8/7/2026, 10:22:17 pm
+// Last updated: 8/7/2026, 10:42:52 pm
 1class Solution {
-2    public int findPairs(int[] nums, int k) {
-3        if (k < 0) return 0;
-4
-5        Arrays.sort(nums);
-6
-7        int i = 0, j = 1;
-8        int c = 0;
-9
-10        while( i < nums.length && j <  nums.length){
-11            if(i == j || nums[j] - nums[i] < k){
-12                j++;
-13            }
-14            else if(nums[j] - nums[i] > k){
-15                i++;
-16            }
-17            else{
-18                i++;
-19                c++;
-20                while(i < nums.length && nums[i] == nums[i - 1]){
-21                    i++;
-22                }
-23            }
-24
-25
-26        }
-27        return c;
-28    }
-29}
+2    public int singleNonDuplicate(int[] nums) {
+3        HashMap<Integer, Integer> map = new HashMap<>();
+4        for (int num : nums) {
+5            map.put(num, map.getOrDefault(num, 0) + 1);
+6        }
+7        for (int num : map.keySet()) {
+8            if(map.get(num) == 1) {
+9                return num;
+10            }
+11        }
+12        return -1;
+13
+14   }
+15}
+16       
+17        
+18    
+19
