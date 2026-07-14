cf27f871e74f7d563c60f4c4ffb19da4a8612abc
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2026-07-14 21:25:45 +0530

    File updated at 14/7/2026, 9:26:28 am

--- 1-TwoSum/1-TwoSum.java
+++ 1-TwoSum/1-TwoSum.java
@@ -1,16 +1,16 @@
-// Last updated: 5/8/2025, 10:26:11 pm
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        Map<Integer,Integer>num_map=new HashMap<>();
-        for(int i=0; i<nums.length; i++){
-            int complement =target-nums[i];
-            if(num_map.containsKey(complement)){
-                return new int[] {num_map.get(complement),i};
-
-            }
-            num_map.put(nums[i],i);
-        }
-        throw new IllegalArgumentException("no match found!");
-        
-    }
-}
+// Last updated: 14/7/2026, 9:26:28 am
+1class Solution {
+2    public int[] twoSum(int[] nums, int target) {
+3        Map<Integer,Integer>num_map=new HashMap<>();
+4        for(int i=0; i<nums.length; i++){
+5            int complement =target-nums[i];
+6            if(num_map.containsKey(complement)){
+7                return new int[] {num_map.get(complement),i};
+8
+9            }
+10            num_map.put(nums[i],i);
+11        }
+12        throw new IllegalArgumentException("no match found!");
+13        
+14    }
+15}
