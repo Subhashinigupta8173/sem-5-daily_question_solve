52c3da01f02ba729bde1c693f721096a7dcbca2c
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2026-07-15 23:43:47 +0530

    File updated at 15/7/2026, 11:44:31 am

--- 64-MinimumPathSum/64-MinimumPathSum.java
+++ 64-MinimumPathSum/64-MinimumPathSum.java
@@ -1,47 +1,26 @@
-// Last updated: 10/8/2025, 10:28:31 pm
-class Solution {
-    public boolean exist(char[][] board, String word) {
-          int m = board.length;
-        int n = board[0].length;
-
-        // Try to start the word from every cell
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (dfs(board, word, i, j, 0)) {
-                    return true;
-                }
-            }
-        }
-
-        return false;
-    }
-
-    private boolean dfs(char[][] board, String word, int i, int j, int index) {
-        // If full word is matched
-        if (index == word.length()) {
-            return true;
-        }
-
-        // Out of bounds or mismatch
-        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length ||
-            board[i][j] != word.charAt(index)) {
-            return false;
-        }
-
-        // Temporarily mark the cell as visited
-        char temp = board[i][j];
-        board[i][j] = '#';
-
-        // Explore all four directions
-        boolean found = dfs(board, word, i + 1, j, index + 1) ||
-                        dfs(board, word, i - 1, j, index + 1) ||
-                        dfs(board, word, i, j + 1, index + 1) ||
-                        dfs(board, word, i, j - 1, index + 1);
-
-        // Restore original character
-        board[i][j] = temp;
-
-        return found;
-        
-    }
-}
+// Last updated: 15/7/2026, 11:44:31 am
+1class Solution {
+2    public int minPathSum(int[][] grid) {
+3        int sum=0;
+4        int m= grid.length;
+5        int n=grid[0].length;
+6
+7        for(int i=0;i<m;i++){
+8            for(int  j=0;j<n;j++){
+9                if(i >0 && j>0){
+10                    grid[i][j]+=Math.min(grid[i-1][j],grid[i][j-1]);
+11                }
+12                else if(i>0){
+13                    grid[i][j]+=grid[i-1][j];
+14                }
+15                else if(j>0){
+16                    grid[i][j]+=grid[i][j-1];
+17
+18                }
+19            }
+20        }
+21        return grid[m-1][n-1];
+22
+23
+24    }
+25}
