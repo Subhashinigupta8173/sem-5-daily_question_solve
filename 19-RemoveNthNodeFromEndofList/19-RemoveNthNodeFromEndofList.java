c42fed0ca4fa04ee6842bdf3119bcd8d13403631
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2026-07-21 18:55:36 +0530

    File updated at 21/7/2026, 6:56:23 am

--- 19-RemoveNthNodeFromEndofList/19-RemoveNthNodeFromEndofList.java
+++ 19-RemoveNthNodeFromEndofList/19-RemoveNthNodeFromEndofList.java
@@ -1,36 +1,36 @@
-// Last updated: 23/9/2025, 6:12:45 am
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    public ListNode removeNthFromEnd(ListNode head, int n) {
-        // Dummy node to handle edge cases
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        ListNode first = dummy;
-        ListNode second = dummy;
-        
-        // Move first n+1 steps ahead
-        for (int i = 0; i <= n; i++) {
-            first = first.next;
-        }
-        
-        // Move both pointers until first reaches end
-        while (first != null) {
-            first = first.next;
-            second = second.next;
-        }
-        
-        // Remove nth node
-        second.next = second.next.next;
-        
-        return dummy.next;
-    }
-}
+// Last updated: 21/7/2026, 6:56:23 am
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11class Solution {
+12    public ListNode removeNthFromEnd(ListNode head, int n) {
+13        
+14        ListNode dummy = new ListNode(0);
+15        dummy.next = head;
+16        ListNode first = dummy;
+17        ListNode second = dummy;
+18        
+19        
+20        for (int i = 0; i <= n; i++) {
+21            first = first.next;
+22        }
+23        
+24       
+25        while (first != null) {
+26            first = first.next;
+27            second = second.next;
+28        }
+29        
+30        // Remove nth node
+31        second.next = second.next.next;
+32        
+33        return dummy.next;
+34    }
+35}
