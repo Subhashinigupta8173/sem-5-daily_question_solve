30686ea9cd9312f3c7c82020bf0d390e1d0b64b8
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2026-08-05 20:47:03 +0530

    File updated at 5/8/2026, 8:47:58 am

--- 11-ContainerWithMostWater/11-ContainerWithMostWater.java
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.java
@@ -1,46 +1,22 @@
-// Last updated: 5/8/2025, 10:26:06 pm
-class Solution {
-    public int maxArea(int[] height) {
-        int maxArea = 0;
-        int left = 0;
-        int right = height.length - 1;
-
-        while (left < right) {
-            int width = right - left;
-            int h = Math.min(height[left], height[right]);
-            int area = width * h;
-            maxArea = Math.max(maxArea, area);
-
-            if (height[left] < height[right]) {
-                left++;
-            } else {
-                right--;
-            }
-        }
-
-        return maxArea;
-    }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println();
-        int n = scanner.nextInt();
-
-        if (n <= 0) {
-            System.out.println();
-            return;
-        }
-
-        int[] height = new int[n];
-
-        System.out.println();
-        for (int i = 0; i < n; i++) {
-            height[i] = scanner.nextInt();
-        }
-
-        Solution solution = new Solution();
-        int result = solution.maxArea(height);
-
-        System.out.println( result);
-    }
-}
+// Last updated: 5/8/2026, 8:47:58 am
+1class Solution {
+2    public int maxArea(int[] height) {
+3        int i = 0;
+4        int j = height.length - 1;
+5        int max = 0;
+6
+7        while (i < j) {
+8            int minHeight = Math.min(height[i], height[j]);
+9            int area = minHeight * (j - i);
+10            max = Math.max(max, area);
+11
+12            // move the smaller height
+13            if (height[i] < height[j]) {
+14                i++;
+15            } else {
+16                j--;
+17            }
+18        }
+19        return max;
+20    }
+21}
