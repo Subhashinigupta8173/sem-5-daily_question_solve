ca7e6792010ef89332ecaba29ef37c8a369410d0
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2026-08-10 09:32:34 +0530

    File updated at 9/8/2026, 9:33:32 pm

--- 713-SubarrayProductLessThanK/713-SubarrayProductLessThanK.java
+++ 713-SubarrayProductLessThanK/713-SubarrayProductLessThanK.java
@@ -1,25 +1,25 @@
-// Last updated: 5/8/2025, 10:24:18 pm
-class Solution {
-    public int numSubarrayProductLessThanK(int[] nums, int k) {
-        int p=1;
-		int si=0;
-		int ei=0;
-		int ans=0;
-		while(ei<nums.length) {
-			//grow
-			p=p*nums[ei];
-			//shrink
-			while(p>=k && si<=ei) {
-				p=p/nums[si];
-				si++;
-				
-			}
-			//ans update
-			ans=ans+(ei-si+1);
-			ei++;
-		}
-		return ans;
-        
-    }
-
-}
+// Last updated: 9/8/2026, 9:33:32 pm
+1class Solution {
+2    public int numSubarrayProductLessThanK(int[] nums, int k) {
+3        int n = nums.length;
+4        int c = 0;
+5        for(int i = 0 ;i < n; i++){
+6            int p =1;
+7           
+8            for(int  j = i; j < n;j++){
+9                p = p*nums[j];
+10                if(p < k ){
+11                    c++;
+12                }
+13                else{
+14                    break;
+15                }
+16                
+17            }
+18
+19        }
+20        return c;
+21
+22        
+23    }
+24}
