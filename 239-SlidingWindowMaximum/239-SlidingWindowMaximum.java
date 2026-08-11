7479f81bc5f8f2105935d3818ad3a5b840e368df
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2026-08-11 17:30:02 +0530

    File updated at 11/8/2026, 5:31:00 am

--- 239-SlidingWindowMaximum/239-SlidingWindowMaximum.java
+++ 239-SlidingWindowMaximum/239-SlidingWindowMaximum.java
@@ -1,34 +1,28 @@
-// Last updated: 11/8/2026, 3:54:50 am
+// Last updated: 11/8/2026, 5:31:00 am
 1class Solution {
-2    public int[] maxSlidingWindow(int[] nums, int k) {
+2    public List<String> topKFrequent(String[] words, int k) {
 3
-4        int n = nums.length;
-5        int[] ans = new int[n - k + 1];
-6
-7        PriorityQueue<int[]> pq =
-8            new PriorityQueue<>((a, b) -> b[0] - a[0]);
-9
-10        int si = 0;
-11
-12        for (int ei = 0; ei < n; ei++) {
-13
-14            // value + index
-15            pq.add(new int[]{nums[ei], ei});
-16
-17            // window se bahar wale elements remove
-18            while (pq.peek()[1] < si) {
-19                pq.poll();
-20            }
-21
-22            // window size k
-23            if (ei - si + 1 == k) {
+4        HashMap<String, Integer> map = new HashMap<>();
+5
+6        for (String word : words) {
+7            map.put(word, map.getOrDefault(word, 0) + 1);
+8        }
+9        PriorityQueue<String> pq = new PriorityQueue<>((a, b) -> {
+10            if (!map.get(a).equals(map.get(b))) {
+11                return map.get(b) - map.get(a);
+12            }
+13            return a.compareTo(b);
+14        });
+15        for (String word : map.keySet()) {
+16            pq.add(word);
+17        }
+18
+19        List<String> ans = new ArrayList<>();
+20        while (k > 0) {
+21            ans.add(pq.poll());
+22            k--;
+23        }
 24
-25                ans[si] = pq.peek()[0];
-26
-27                si++;
-28            }
-29        }
-30
-31        return ans;
-32    }
-33}
+25        return ans;
+26    }
+27}
