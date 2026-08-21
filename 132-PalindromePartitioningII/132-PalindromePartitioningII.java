860227f5338b906a1b4b3862d252e4aac2b4d282
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2026-08-21 23:20:29 +0530

    File updated at 21/8/2026, 11:21:32 am

--- 132-PalindromePartitioningII/132-PalindromePartitioningII.java
+++ 132-PalindromePartitioningII/132-PalindromePartitioningII.java
@@ -1,25 +1,21 @@
-// Last updated: 21/8/2026, 10:53:01 am
+// Last updated: 21/8/2026, 11:21:32 am
 1class Solution {
-2    public boolean predictTheWinner(int[] nums) {
-3        int  n = nums.length;
-4        int [] [] dp = new int [n][n];
-5        for(int i = 0; i < n ;i++){
-6            Arrays.fill(dp[i],-1);
-7        }
-8        return solve(nums,0 ,n-1,dp) >= 0;
-9    
-10        
-11
-12    }
-13    public int solve (int [] nums,int i ,int j,int[][]dp){
-14        if( i == j){
-15            return nums[i];
-16        }
-17        if(dp[i][j] != -1){
-18            return dp[i][j];
-19        }
-20        int takei = nums[i] - solve(nums,i+1,j,dp);
-21        int takej = nums[j] - solve(nums,i, j -1,dp );
-22        return dp[i][j] = Math.max(takei,takej);
-23    }
-24}
+2    public int wiggleMaxLength(int[] nums) {
+3        int n = nums.length;
+4        if( n < 2){
+5            return n;
+6        }
+7        int c =  1;
+8        int prevdiff = 0;
+9        for(int  i = 1; i <n;i++){
+10            int diff = nums[i] - nums[i-1];
+11            if((diff > 0 && prevdiff <= 0) || (diff < 0 && prevdiff >= 0 )){
+12                c++;
+13                prevdiff = diff;
+14            }
+15        }
+16        return c;
+17
+18        
+19    }
+20}
