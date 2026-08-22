91121c73c16957eb40407ca9731557fca07a05f8
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2026-08-22 23:27:37 +0530

    File updated at 22/8/2026, 11:28:40 am

--- 413-ArithmeticSlices/413-ArithmeticSlices.java
+++ 413-ArithmeticSlices/413-ArithmeticSlices.java
@@ -1,24 +1,54 @@
-// Last updated: 22/8/2026, 11:14:02 am
+// Last updated: 22/8/2026, 11:28:40 am
 1class Solution {
-2    public int findTargetSumWays(int[] nums, int target) {
-3        int sum = 0;
-4        for (int i = 0; i < nums.length; i++) {
-5            sum += nums[i];
-6        }
-7        if (Math.abs(target) > sum) {
-8            return 0;
-9        }
-10        if ((sum + target) % 2 != 0) {
-11            return 0;
+2    public boolean makesquare(int[] matchsticks) {
+3
+4        int sum = 0;
+5
+6        for (int x : matchsticks) {
+7            sum += x;
+8        }
+9
+10        if (sum % 4 != 0) {
+11            return false;
 12        }
-13        int s = (sum + target) / 2;
-14        int[] dp = new int[s + 1];
-15        dp[0] = 1;
-16        for (int i = 0; i < nums.length; i++) {
-17            for (int j = s; j >= nums[i]; j--) {
-18                dp[j] = dp[j] + dp[j - nums[i]];
-19            }
-20        }
-21        return dp[s];
-22    }
-23}
+13
+14        int target = sum / 4;
+15
+16        int[] side = new int[4];
+17
+18        return solve(matchsticks, 0, target, side);
+19    }
+20
+21    public boolean solve(int[] matchsticks, int index, int target, int[] side) {
+22
+23        if (index == matchsticks.length) {
+24            return side[0] == target &&
+25                   side[1] == target &&
+26                   side[2] == target &&
+27                   side[3] == target;
+28        }
+29
+30        int stick = matchsticks[index];
+31
+32        for (int i = 0; i < 4; i++) {
+33
+34            if (side[i] + stick <= target) {
+35
+36                side[i] += stick;
+37
+38                if (solve(matchsticks, index + 1, target, side)) {
+39                    return true;
+40                }
+41
+42                side[i] -= stick;
+43            }
+44
+45           
+46            if (side[i] == 0) {
+47                break;
+48            }
+49        }
+50
+51        return false;
+52    }
+53}
