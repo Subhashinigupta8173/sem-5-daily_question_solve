85d5c189b480ac7f1bd2304bf6de75c2baa4eb2e
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2026-08-23 00:12:47 +0530

    File updated at 22/8/2026, 12:13:50 pm

--- 413-ArithmeticSlices/413-ArithmeticSlices.java
+++ 413-ArithmeticSlices/413-ArithmeticSlices.java
@@ -1,50 +1,24 @@
-// Last updated: 22/8/2026, 12:08:02 pm
+// Last updated: 22/8/2026, 12:13:50 pm
 1class Solution {
-2    List<Integer> ll;
+2    int[] dp;
 3
-4    public boolean makesquare(int[] matchsticks) {
-5        int sum = 0;
-6
-7        ll = new ArrayList<>();
-8
-9        for (int x : matchsticks) {
-10            sum += x;
-11            ll.add(x);
-12        }
-13
-14        if (sum % 4 != 0) {
-15            return false;
-16        }
-17
-18        int tar = sum / 4;
-19
-20        Collections.sort(ll, Collections.reverseOrder());
-21
-22        return solve(0, 0, 0, 0, 0, tar);
-23    }
-24
-25    public boolean solve(int s1, int s2, int s3, int s4, int i, int tar) {
-26
-27        if (s1 > tar || s2 > tar || s3 > tar || s4 > tar) {
-28            return false;
-29        }
-30
-31        if (i == ll.size()) {
-32            if (s1 == tar && s2 == tar && s3 == tar && s4 == tar) {
-33                return true;
-34            }
-35            return false;
-36        }
-37
-38        if (
-39            solve(s1 + ll.get(i), s2, s3, s4, i + 1, tar) ||
-40            solve(s1, s2 + ll.get(i), s3, s4, i + 1, tar) ||
-41            solve(s1, s2, s3 + ll.get(i), s4, i + 1, tar) ||
-42            solve(s1, s2, s3, s4 + ll.get(i), i + 1, tar)
-43        ) {
-44            return true;
-45        }
-46
-47        return false;
-48    }
-49}
+4    public int fib(int n) {
+5        dp = new int[n + 1];
+6        Arrays.fill(dp, -1);
+7
+8        return solve(n);
+9    }
+10
+11    public int solve(int n) {
+12        if (n == 0) {
+13            return 0;
+14        }
+15        if (n == 1) {
+16            return 1;
+17        }
+18        if (dp[n] != -1) {
+19            return dp[n];
+20        }
+21        return dp[n] = solve(n - 1) + solve(n - 2);
+22    }
+23}
