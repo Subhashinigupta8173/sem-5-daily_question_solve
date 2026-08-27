fbca48eeb5c86ae8a1be30d921ff9762b9fa369c
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2026-08-27 10:50:50 +0530

    File updated at 26/8/2026, 10:51:55 pm

--- 455-AssignCookies/455-AssignCookies.java
+++ 455-AssignCookies/455-AssignCookies.java
@@ -1,25 +1,28 @@
-// Last updated: 1/7/2026, 9:38:41 pm
+// Last updated: 26/8/2026, 10:51:55 pm
 1class Solution {
-2    public int findContentChildren(int[] arr, int[] s) {
-3        int n=arr.length;
-4        int m=s.length;
-5        int c=0;
-6        int i=0;
-7        int j=0;
-8        Arrays.sort(s);
-9        Arrays.sort(arr);
-10        while(i<n && j<m){
-11           
-12                if(s[j]>=arr[i] ){
-13                    c++;
-14                    i++;
-15                    j++;
-16                
+2    public int findContentChildren(int[] g, int[] s) {
+3        int n = g.length;
+4        int m = s.length;
+5        int c = 0;
+6        int j = 0;
+7        if(m == 0 || n == 0){
+8            return 0;
+9        }
+10        Arrays.sort(g);
+11        Arrays.sort(s);
+12
+13        for (int i = 0; i < n && j < m; i++) {
+14            if (s[j] >= g[i]) {
+15                c++;
+16                j++;
 17            }
 18            else{
 19                j++;
-20            }
-21        }
-22        return c;
-23    }
-24}
+20                i--;
+21            }
+22        }
+23        return c;
+24
+25        
+26    }
+27}
