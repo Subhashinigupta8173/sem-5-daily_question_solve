abb4f06ed717ddb9a1ba4a9578bc329189785fcb
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2026-08-29 19:44:38 +0530

    File updated at 29/8/2026, 7:45:44 am

--- 200-NumberofIslands/200-NumberofIslands.java
+++ 200-NumberofIslands/200-NumberofIslands.java
@@ -1,39 +1,41 @@
-// Last updated: 27/7/2026, 8:09:29 pm
+// Last updated: 29/8/2026, 7:45:44 am
 1class Solution {
-2    int[] dr = {-1, 1, 0, 0};
-3    int[] dc = {0, 0, -1, 1};
-4    public int numIslands(char[][] grid) {
+2    boolean [][] visited;
+3    public int numIslands(char[][] grid) {
+4        
 5        int m = grid.length;
 6        int n = grid[0].length;
-7        int islands = 0;
-8        Queue<int []> q = new LinkedList<>();
-9        for(int i = 0; i < m ; i++){
-10            for(int j = 0;j < n;j++){
-11                if(grid[i][j] == '1'){
-12                    islands++;
-13                    grid[i][j] = '0';
-14                    q.offer(new int []{i,j});
-15                    while(!q.isEmpty()){
-16                        int [] curr = q.poll();
-17                        int r = curr[0];
-18                        int c = curr[1];
-19                        for(int k = 0 ; k < 4 ; k++){
-20                            int nr = r + dr[k];
-21                            int nc = c + dc[k];
-22                            if(nc >= 0 && nc < n && nr >= 0 && nr < m && grid[nr][nc] ==  '1'){
-23                                grid[nr][nc] = '0';
-24                                q.offer(new int[]{nr,nc});
-25                            }
-26                        }
-27
-28                    }
-29                }
+7        int count = 0 ;
+8        int[] dc = {-1, 1, 0, 0};
+9        int[] dr = {-0 ,0, -1, 1};
+10        Queue<int[]>q = new LinkedList<>();
+11        for(int i  =0;i < m;i++){
+12            for(int j = 0; j < n;j++){
+13                if(grid[i][j] == '1'){
+14                    grid[i][j] = 'A';
+15                    count++;
+16                    q.offer(new  int []{i,j});
+17                    while(!q.isEmpty()){
+18                        int [] top = q.poll();
+19                        int r = top[0];
+20                        int c = top[1];
+21                        for(int k  = 0;k<4;k++){
+22                            int nr = r + dr[k];
+23                            int nc = c + dc[k];
+24                            if(nr>= 0 && nr < m && nc >= 0 && nc < n && grid[nr][nc] == '1'){
+25                                q.offer(new int[]{nr,nc});
+26                                grid[nr][nc] = 'A';
+27                            }
+28                        }
+29                    }
 30
-31            }
-32        }
-33        return islands;
-34
+31                }
+32            }
+33        }
+34        return count;
 35
-36        
+36
 37    }
-38}
+38    
+39
+40}
