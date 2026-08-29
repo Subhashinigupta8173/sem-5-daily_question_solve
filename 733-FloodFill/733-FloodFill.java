f62be1a8a9e504107d70da3614534319ba89b05b
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2026-08-29 19:09:32 +0530

    File updated at 29/8/2026, 7:10:39 am

--- 733-FloodFill/733-FloodFill.java
+++ 733-FloodFill/733-FloodFill.java
@@ -1,33 +1,36 @@
-// Last updated: 27/7/2026, 8:27:04 am
+// Last updated: 29/8/2026, 7:10:39 am
 1class Solution {
 2    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-3        int m = image.length;
-4        int n = image[0].length;
-5        int originalColor = image[sr][sc];
-6         if (originalColor == color)
-7            return image;
-8
-9        Queue<int []>q = new LinkedList<>();
-10        
-11        q.offer(new int []{sr,sc});
-12        image[sr][sc] = color;
-13        int [] dr = {-1,1,0,0};
-14        int [] dc=  {0,0,-1,1};
-15        while(!q.isEmpty()){
-16
-17            int[] curr =  q.poll();
-18            int r = curr[0];
-19            int c = curr[1];
-20            for(int i = 0; i < 4; i++){
-21                int nr = r + dr[i];
-22                int nc = c + dc[i];
-23                if(nr >= 0 && nr < m && nc >= 0 && nc < n && image[nr][nc] == originalColor ){
-24                    image[nr][nc] = color;
-25                    q.offer(new int []{nr,nc});
-26                }
+3    int n = image.length;
+4    int m = image[0].length;
+5    
+6
+7    Queue<int[]> q = new LinkedList<>();
+8    q.offer(new int[]{sr,sc});
+9    int pixel =image[sr][sc];
+10    image[sr][sc] = color;
+11    int [] dr = {-1,1,0,0};
+12    int [] dc = {0,0,-1,1};
+13    if(pixel == color){
+14        return image;
+15    }
+16    while(!q.isEmpty()){
+17        int [] curr = q.poll();
+18        int r = curr[0];
+19        int c = curr[1];
+20        for(int i = 0; i<4; i++){
+21            int nr = r + dr[i];
+22            int nc = c + dc[i];
+23            if(nc >= 0 && nc< m && nr >=0 && nr < n && image[nr][nc] == pixel ){
+24                q.offer(new int[]{nr,nc});
+25                image[nr][nc] = color;
+26
 27            }
-28
-29        }
-30        return image;
-31    }
-32}
+28        }
+29    }
+30    return image;
+31
+32
+33        
+34    }
+35}
