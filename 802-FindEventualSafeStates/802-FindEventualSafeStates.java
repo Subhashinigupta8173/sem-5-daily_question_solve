ddc0fae9850ce90e745e9e7d265eb19c1d177f3f
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2026-08-30 11:07:12 +0530

    File updated at 29/8/2026, 11:08:18 pm

--- 802-FindEventualSafeStates/802-FindEventualSafeStates.java
+++ 802-FindEventualSafeStates/802-FindEventualSafeStates.java
@@ -1,41 +1,41 @@
-// Last updated: 28/7/2026, 2:36:29 am
+// Last updated: 29/8/2026, 11:08:18 pm
 1class Solution {
 2    public List<Integer> eventualSafeNodes(int[][] graph) {
 3        int n = graph.length;
-4        List<Integer>[] rev = new ArrayList[n];
-5        for(int i = 0; i<n;i++){
-6            rev[i]=new ArrayList<>();
+4        ArrayList<Integer>[] reverse = new ArrayList[n];
+5        for (int i = 0; i < n; i++) {
+6            reverse[i] = new ArrayList<>();
 7        }
-8        int [] outdegree = new int [n];
-9        for(int i =0; i<n;i++){
-10            outdegree[i] = graph[i].length;
-11            for(int nbrs : graph[i]){
-12                rev[nbrs].add(i) ;//reverse edge create kr rha haiye,rev[1].add(0)
-13            }
-14        }
-15        Queue<Integer>q = new LinkedList<>();
-16        for(int i=0;i<n;i++){
-17            if(outdegree[i] == 0){
-18                q.offer(i);
-19            }
-20        }
-21
-22        List<Integer> ans = new ArrayList<>();
-23        while(!q.isEmpty()){
-24            int node = q.poll();//outdegree 0 vale phle se hi node me hai jo ki termnial hai 
-25            ans.add(node);//isliye ans me directadd kar diya
-26            for(int parent: rev[node]){
-27                outdegree[parent]--;
-28                if(outdegree[parent] == 0){
-29                    q.offer(parent);
-30                }
-31            }
-32        }
-33        Collections.sort(ans);
-34        return ans;
-35
-36         
-37
-38        
+8        ArrayList<Integer> ans = new ArrayList<>();
+9        int[] outdegree = new int[n];
+10        for (int i = 0; i < n; i++) {
+11            outdegree[i] = graph[i].length;
+12            for (int nbrs : graph[i]) {
+13                reverse[nbrs].add(i);
+14            }
+15
+16        }
+17        Queue<Integer> q = new LinkedList<>();
+18
+19        for (int i = 0; i < n; i++) {
+20            if (outdegree[i] == 0) {
+21                q.add(i);
+22            }
+23        }
+24        while (!q.isEmpty()) {
+25            int curr = q.poll();
+26            ans.add(curr);
+27            for (int node : reverse[curr]) {
+28                outdegree[node]--;
+29                if (outdegree[node] == 0) {
+30                    q.add(node);
+31
+32                }
+33            }
+34
+35        }
+36        Collections.sort(ans);
+37        return ans;
+38
 39    }
 40}
