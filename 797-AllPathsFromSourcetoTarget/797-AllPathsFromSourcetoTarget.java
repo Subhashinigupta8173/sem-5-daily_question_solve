e72290c99d1bb1081f1946d1d0f9c43c08d51bba
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2026-08-30 10:39:54 +0530

    File updated at 29/8/2026, 10:41:00 pm

--- 797-AllPathsFromSourcetoTarget/797-AllPathsFromSourcetoTarget.java
+++ 797-AllPathsFromSourcetoTarget/797-AllPathsFromSourcetoTarget.java
@@ -1,27 +1,23 @@
-// Last updated: 29/8/2026, 10:40:15 pm
+// Last updated: 29/8/2026, 10:41:00 pm
 1class Solution {
-2    List<List<Integer>> ans = new ArrayList<>();
+2    List<List<Integer>>  ans = new ArrayList<>();
 3    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-4
-5        List<Integer> ways = new LinkedList<>();
-6        ways.add(0);
-7        dfs(0, graph, ways);
-8
-9        return ans;
-10
-11        
-12    }
-13    private void dfs(int node,int[][] graph,List<Integer> ways){
-14        if(node == graph.length - 1){
-15            ans.add(new ArrayList<>(ways));
-16            return ;
-17        }
-18        for(int nbrs : graph[node]){
-19            ways.add(nbrs);
-20            dfs(nbrs,graph,ways);
-21            ways.remove(ways.size()-1);
-22
-23        }
-24    }
-25
-26}
+4        List<Integer>  ways = new ArrayList<>();
+5        ways.add(0);
+6        dfs(0,graph,ways);
+7        return ans;
+8    
+9        
+10    }
+11    public  void dfs(int i,int [][] graph,List<Integer> ways){
+12        if(i == graph.length-1){
+13            ans.add(new ArrayList<>(ways));
+14            return ;
+15        }
+16        for(int nbrs : graph[i]){
+17            ways.add(nbrs);
+18            dfs(nbrs,graph,ways);
+19            ways.remove(ways.size()-1);
+20        }
+21    }
+22}
