0d017e5fd4a60f6f649637f47120bf74e3137868
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2026-09-01 21:45:46 +0530

    File updated at 1/9/2026, 9:46:54 am

--- 96-UniqueBinarySearchTrees/96-UniqueBinarySearchTrees.java
+++ 96-UniqueBinarySearchTrees/96-UniqueBinarySearchTrees.java
@@ -1,16 +1,17 @@
-// Last updated: 23/9/2025, 11:42:17 pm
-class Solution {
-    public int numTrees(int n) {
-        int[] dp = new int[n + 1];
-        dp[0] = 1; // empty tree
-        dp[1] = 1; // one node
-
-        for (int nodes = 2; nodes <= n; nodes++) {
-            for (int root = 1; root <= nodes; root++) {
-                dp[nodes] += dp[root - 1] * dp[nodes - root];
-            }
-        }
-
-        return dp[n];
-    }
-}
+// Last updated: 1/9/2026, 9:46:54 am
+1class Solution {
+2    public int numTrees(int n) {
+3        int[] dp = new int[n + 1];
+4        dp[0] = 1; // empty tree
+5        dp[1] = 1; // one node
+6
+7        for (int nodes = 2; nodes <= n; nodes++) {
+8            for (int root = 1; root <= nodes; root++) {
+9                dp[nodes] += dp[root - 1] * dp[nodes - root];
+10            }
+11        }
+12
+13        return dp[n];
+14    }
+15}
+16
