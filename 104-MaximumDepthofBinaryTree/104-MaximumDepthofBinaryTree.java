044137f9b4e7c5bd34e9bf9d25c869cefb974fa0
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2026-09-10 18:59:57 +0530

    Create 104-MaximumDepthofBinaryTree

---
+++ 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.java
@@ -0,0 +1,29 @@
+// Last updated: 10/9/2026, 6:59:58 pm
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public int maxDepth(TreeNode root) {
+18
+19        if (root == null) {
+20            return 0;
+21        }
+22
+23        int left = maxDepth(root.left);
+24        int right = maxDepth(root.right);
+25
+26        return 1 + Math.max(left, right);
+27    }
+28}
