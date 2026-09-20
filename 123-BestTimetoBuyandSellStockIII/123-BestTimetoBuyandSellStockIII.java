3ae041d44d195376da720433f9993923b4fcf30d
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2026-09-20 23:23:16 +0530

    File updated at 20/9/2026, 11:23:14 pm

--- 123-BestTimetoBuyandSellStockIII/123-BestTimetoBuyandSellStockIII.java
+++ 123-BestTimetoBuyandSellStockIII/123-BestTimetoBuyandSellStockIII.java
@@ -1,27 +1,27 @@
-// Last updated: 20/9/2026, 10:15:31 pm
+// Last updated: 20/9/2026, 11:23:14 pm
 1class Solution {
 2    public int maxProfit(int[] prices) {
 3        int n = prices.length;
-4
-5        int[] suffixProfit = new int[n];
-6
-7        int maxPrice = prices[n - 1];
-8
-9        for (int i = n - 1; i >= 0; i--) {
-10            maxPrice = Math.max(maxPrice, prices[i]);
-11            suffixProfit[i] = maxPrice - prices[i];
-12        }
-13
-14        int minPrice = prices[0];
-15        int prefixProfit = 0;
-16        int answer = suffixProfit[0];
-17
-18        for (int i = 1; i < n; i++) {
-19            prefixProfit = Math.max(prefixProfit, prices[i] - minPrice);
-20            minPrice = Math.min(minPrice, prices[i]);
-21            answer = Math.max(answer, prefixProfit + suffixProfit[i]);
+4        int [] left  = new int [n];
+5        int [] right = new int [n];
+6        int minPrice  = prices[0];
+7        for(int i = 1; i < n ;i++){
+8            minPrice = Math.min(minPrice,prices[i]);
+9            left[i] = Math.max(left[i - 1],prices[i] - minPrice);
+10
+11        }
+12        int maxPrice = prices[n - 1];
+13        for(int i = n - 2; i >= 0; i--){
+14            maxPrice = Math.max(maxPrice, prices[i]);
+15            right[i] = Math.max(right[i + 1],maxPrice - prices[i]);
+16
+17        }
+18        int ans  = 0;
+19        for(int i = 0; i < n; i++){
+20            ans =  Math.max(ans,left[i] + right[i]);
+21
 22        }
-23
-24        return answer;
+23        return ans;
+24       
 25    }
 26}
