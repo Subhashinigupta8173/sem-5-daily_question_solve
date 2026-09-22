a8cdccea9c95caf64d6de6787dd12e0bb95d6d30
Subhashinigupta8173 <156153718+Subhashinigupta8173@users.noreply.github.com>
2026-09-22 16:07:26 +0530

    File updated at 22/9/2026, 4:07:25 pm

--- 35-SearchInsertPosition/35-SearchInsertPosition.java
+++ 35-SearchInsertPosition/35-SearchInsertPosition.java
@@ -1,47 +1,23 @@
-// Last updated: 5/8/2025, 10:25:41 pm
-import java.util.Scanner;
-import java.util.Arrays;
-
-class Solution {
-    public int searchInsert(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2; // Avoid potential overflow
-
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-
-        return left;
-    }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter the number of elements in the array:");
-        int n = scanner.nextInt();
-        int[] nums = new int[n];
-
-        System.out.println("Enter the elements of the array:");
-        for (int i = 0; i < n; i++) {
-            nums[i] = scanner.nextInt();
-        }
-
-        System.out.println("Enter the target value:");
-        int target = scanner.nextInt();
-
-        Solution solution = new Solution();
-        int index = solution.searchInsert(nums, target);
-        System.out.println("Output: " + index);
-
-        System.out.println("Array after search insert:");
-        System.out.println(Arrays.toString(nums));
-    }
-}
+// Last updated: 22/9/2026, 4:07:25 pm
+1class Solution {
+2    public int searchInsert(int[] nums, int target) {
+3        int low = 0;
+4        int high  = nums.length - 1;
+5        while(low <= high){
+6            int mid = low + (high - low) / 2;
+7            if(nums[mid] == target){
+8                return mid;
+9            }
+10            else if(nums[mid] < target){
+11                low= mid + 1;
+12
+13            }
+14            else{
+15                high = mid - 1;
+16            }
+17        }
+18        return low;
+19
+20        
+21    }
+22}
